e54f40f159de4da3776d5798cebba5def48d31a0
cazdemun <cplu1509@gmail.com>
2021-09-09 15:29:13 -0500

    fix wrong prop on image

--- src/views/Mobile.vue
+++ src/views/Mobile.vue
@@ -3,7 +3,7 @@
     <v-row class="mx-0">
       <v-col cols="12" class="d-flex flex-column align-center py-0">
         <v-img position="center center" src="@/assets/mobile/logo_mobile.png"
-               v-bind:width="[$vuetify.breakpoint.smOnly ? 310 : 182]" contain/>
+               v-bind:width="$vuetify.breakpoint.smOnly ? 310 : 182" contain/>
       </v-col>
       <v-col cols="12" class="text-center d-flex flex-column align-center py-0">
         <div v-bind:class="[$vuetify.breakpoint.smOnly ? 'title-warning-sm' : 'title-warning-xs']">
