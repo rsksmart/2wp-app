e2d51d6dbd659334f7e4764154cbb18fcf740b21
cazdemun <cplu1509@gmail.com>
2021-09-16 08:24:56 -0500

    reuse rlogin instance

--- src/store/session/actions.ts
+++ src/store/session/actions.ts
@@ -8,8 +8,8 @@ import { Web3SessionState } from './types';
 import { RootState } from '../types';
 
 export const actions: ActionTree<Web3SessionState, RootState> = {
-  [constants.SESSION_CONNECT_WEB3]: async ({ commit }) => {
-    const rLogin = new RLogin({
+  [constants.SESSION_CONNECT_WEB3]: async ({ commit, state }) => {
+    const rLogin = state.rLoginInstance === undefined ? new RLogin({
       cacheProvider: false,
       providerOptions: {
         walletconnect: {
@@ -23,25 +23,19 @@ export const actions: ActionTree<Web3SessionState, RootState> = {
         },
       },
       supportedChains: [30, 31],
-    });
+    }) : state.rLoginInstance;
     rLogin.connect()
       .then((rLoginResponse) => {
         const web3 = new Web3(rLoginResponse.provider);
         Vue.prototype.$web3 = web3;
         commit(constants.SESSION_IS_ENABLED, true);
         commit(constants.SESSION_SET_RLOGIN, rLoginResponse);
+        commit(constants.SESSION_SET_RLOGIN_INSTANCE, rLogin);
         commit(constants.SESSION_SET_WEB3_INSTANCE, web3);
-        const rLoginModal = document.getElementById('rlogin-connect-modal');
-        if (rLoginModal !== null && rLoginModal.parentNode !== null) {
-          rLoginModal.parentNode.removeChild(rLoginModal);
-        }
       })
       .catch(() => {
         commit(constants.SESSION_IS_ENABLED, false);
-        const rLoginModal = document.getElementById('rlogin-connect-modal');
-        if (rLoginModal !== null && rLoginModal.parentNode !== null) {
-          rLoginModal.parentNode.removeChild(rLoginModal);
-        }
+        commit(constants.SESSION_SET_RLOGIN_INSTANCE, rLogin);
       });
   },
   [constants.WEB3_SESSION_GET_ACCOUNT]: async ({ commit }) => {
--- src/store/session/types.ts
+++ src/store/session/types.ts
@@ -1,3 +1,5 @@
+import RLogin from '@rsksmart/rlogin';
+
 export interface Web3SessionState {
   enabled: boolean;
   account?: string;
@@ -5,4 +7,5 @@ export interface Web3SessionState {
   rLogin?: {
     disconnect: () => Promise<void>;
   };
+  rLoginInstance?: RLogin;
 }
