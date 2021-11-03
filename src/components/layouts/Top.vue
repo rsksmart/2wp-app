ee99f54b957c1790758c123f8eae59b592454b36
cazdemun <cplu1509@gmail.com>
2021-11-03 09:51:01 -0500

    fix logo alignment and pointer

--- src/components/layouts/Top.vue
+++ src/components/layouts/Top.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-app-bar color="#fff" elevation="0" class="mx-8">
+  <v-app-bar color="#fff" elevation="0" class="mx-8 top">
     <v-row justify="center">
-      <v-col cols="11" class="pb-0 pt-10 pr-0">
+      <v-col cols="11" class="pb-0 pt-10 pl-2 pr-0">
             <v-img @click="toExchange" position="center left"
                    src="@/assets/logo-beta.svg"
                    alt="RSK Two Way Peg"
-                   height="65" contain class="rsk-main-logo"/>
+                   height="65" width="180" contain class="rsk-main-logo"/>
       </v-col>
     </v-row>
   </v-app-bar>
