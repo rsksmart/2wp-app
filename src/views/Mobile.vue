5ae5f1f37b93330b30a9b9fbe0aa177db4586a11
jeremy-then <jeremy.then@iovlabs.org>
2022-01-20 00:16:39 -0400

    Fixed mobile logo to match the main logo

--- src/views/Mobile.vue
+++ src/views/Mobile.vue
@@ -2,7 +2,7 @@
   <v-container v-if="$vuetify.breakpoint.smAndDown" class="mobile" fluid>
     <v-row class="mobile-content mx-0">
       <v-col class="py-0">
-        <v-img position="center center" src="@/assets/mobile/logo_mobile.png"
+        <v-img position="center center" src="@/assets/logo-beta.svg"
                v-bind:height="$vuetify.breakpoint.smOnly ? 175 : 105" contain/>
       </v-col>
       <v-col cols="12" class="text-center d-flex flex-column align-center py-0">
