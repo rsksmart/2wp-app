81ef7be05604149f528e3a24ebc1bd1991646157
Ronald Sarmiento <roasarmientoga@unal.edu.co>
2022-02-09 11:13:32 -0500

    fixing linebreak operator linter issue

--- src/App.vue
+++ src/App.vue
@@ -30,14 +30,14 @@ export default class App extends Vue {
   mounted() {
     this.scriptTag = document.createElement('script');
     this.scriptTag.type = 'text/javascript';
-    this.scriptTag.text = '(function(h,o,t,j,a,r){' +
-      'h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};' +
-      `h._hjSettings={hjid:'${process.env.VUE_APP_HOTJAR_ID}',hjsv:6};` +
-      'a=o.getElementsByTagName("head")[0];' +
-      'r=o.createElement("script");r.async=1;' +
-      'r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;' +
-      'a.appendChild(r);' +
-    '})(window,document,"https://static.hotjar.com/c/hotjar-",".js?sv=");';
+    this.scriptTag.text = '(function(h,o,t,j,a,r){'
+      + 'h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};'
+      + `h._hjSettings={hjid:'${process.env.VUE_APP_HOTJAR_ID}',hjsv:6};`
+      + 'a=o.getElementsByTagName("head")[0];'
+      + 'r=o.createElement("script");r.async=1;'
+      + 'r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;'
+      + 'a.appendChild(r);'
+    + '})(window,document,"https://static.hotjar.com/c/hotjar-",".js?sv=");';
 
     document.body.appendChild(this.scriptTag);
   }
