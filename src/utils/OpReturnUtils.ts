878ed8c9f1cca90288359f8ecc41a6661040ebba
Ronald Sarmiento <11847044+ronaldsg20@users.noreply.github.com>
2022-06-06 11:10:43 -0500

    Update src/utils/OpReturnUtils.ts
    Co-authored-by: josedahlquist <jose.dahlquist@gmail.com>

--- src/utils/OpReturnUtils.ts
+++ src/utils/OpReturnUtils.ts
@@ -67,7 +67,7 @@ export function isValidOpReturn(
   for (let i = 0; outputs && i < outputs.length; i += 1) {
     const output: NormalizedOutput = outputs[i];
     if (output.op_return_data
-      && output.op_return_data.length >= 50
+      && (output.op_return_data.length === 50 || output.op_return_data.length === 92)
       && output.op_return_data.substr(0, 10).startsWith('52534b5401')
     ) { // Includes version 01 in the same if
       const opReturnDestAddress = output.op_return_data.substring(10, 50);
