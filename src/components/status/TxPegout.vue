6b2d72d7bfde6f81ace38097b769e3536ef49fa4
jeremy-then <jeremy.then@iovlabs.org>
2022-06-27 16:14:20 -0400

    Update transaction status view
    - now uses new endpoint tx-status
    - prepare code to work with pegout-status
    - show pegout-status as invalid pegin-status for the moment

---
+++ src/components/status/TxPegout.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-col>
+      <v-row justify="center" class="mt-6">
+        <div v-if="pegStatus">Pegout status: {{pegStatus.status}}</div>
+      </v-row>
+  </v-col>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+  Emit,
+} from 'vue-property-decorator';
+import { setStatusMessage } from '@/services/utils';
+import {
+  PegoutStatusDataModel,
+  TxStatusType,
+} from '@/types';
+
+@Component({})
+export default class TxPegout extends Vue {
+  @Prop() pegStatus!: PegoutStatusDataModel;
+
+  @Emit('setMessage')
+  setMessage() {
+    return setStatusMessage(TxStatusType.PEGOUT, this.pegStatus.status);
+  }
+
+  created() {
+    this.setMessage();
+  }
+}
+</script>
