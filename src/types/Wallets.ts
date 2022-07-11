ede036483c1ad2ac08e219afda09b98ccaa9f51a
Ronald Sarmiento <roasarmientoga@unal.edu.co>
2022-07-11 00:42:38 -0500

    Xpub utils
                    I've updated the utils package in order to include xpub
                    derivation utils

--- src/types/Wallets.ts
+++ src/types/Wallets.ts
@@ -2,3 +2,10 @@ import { TrezorSignedTx } from '@/types/Trezor';
 import { LedgerSignedTx } from '@/types/Ledger';
 
 export type SignedTx = TrezorSignedTx | LedgerSignedTx;
+
+// eslint-disable-next-line no-shadow
+export enum Purpose {
+  P2PKH = '44',
+  P2SH = '49',
+  P2WPKH = '84',
+}
