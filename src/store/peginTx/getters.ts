34cfc8c1e6c484a30fccd417c372dca4afeb393e
Ronald Sarmiento <roasarmientoga@unal.edu.co>
2022-07-15 15:11:48 -0500

    Updating StartAskingForBalance
                    I've change the way addresses are got from HDwallets in
                    the startAskingForBalance method, asking and using the
                    xpub value to derive them.

--- src/store/peginTx/getters.ts
+++ src/store/peginTx/getters.ts
@@ -124,4 +124,19 @@ export const getters: GetterTree<PegInTxState, RootState> = {
     },
   [constants.PEGIN_TX_GET_WALLET_SERVICE]:
     (state: PegInTxState): WalletService | undefined => state.walletService,
+  [constants.PEGIN_TX_IS_HD_WALLET]: (state: PegInTxState): boolean => {
+    let isHdWallet = false;
+    switch (state.bitcoinWallet) {
+      case constants.WALLET_TREZOR:
+        isHdWallet = true;
+        break;
+      case constants.WALLET_LEDGER:
+        isHdWallet = true;
+        break;
+      default:
+        isHdWallet = false;
+        break;
+    }
+    return isHdWallet;
+  },
 };
