e354727fc1b1d8ea37142b0c14020ff1ff4e971c
gsus <barrazajesusarmando@gmail.com>
2022-07-25 12:03:20 -0500

    error component

---
+++ src/components/status/TxPegout.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-col>
+      <v-row justify="center" class="mt-6">
+        <div v-if="pegStatus">Pegout status: {{pegStatus.status}}</div>
+      </v-row>
+  </v-col>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'vue-property-decorator';
+import {
+  PegoutStatusDataModel,
+} from '@/types';
+
+@Component({})
+export default class TxPegout extends Vue {
+  @Prop() pegStatus!: PegoutStatusDataModel;
+
+  created() {
+    console.log('>>>>>', this.pegStatus);
+  }
+}
+</script>
