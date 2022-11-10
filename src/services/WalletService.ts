9c04e62ee468f061609045804e6a9e7b4c7171e4
Alex Braz <alexjavabraz@gmail.com>
2022-11-10 10:01:38 -0300

    Revert "update inform to subscribers only when are no exceptions"
    This reverts commit 59bf38971cf33f1e64a5db4fa6dcb022fa74eb9b.

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -193,10 +193,12 @@ export default abstract class WalletService {
           return;
         }
       }
-      this.loadingBalances = false;
       this.informSubscribers(this.balanceAccumulated, []);
-    } catch (error: any) {
-      throw new Error(`Balance Error: ${error.message}`);
+    } catch (error) {
+      if (!error.message) {
+        error.message = 'Error fetching balance';
+      }
+      throw error;
     } finally {
       this.loadingBalances = false;
     }
