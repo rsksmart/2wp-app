691bc079d285672e515088eb4514a5e700ac78d5
hard75 <barrazajesusarmando@gmail.com>
2022-11-15 16:28:01 -0500

    page pegout

---
+++ src/views/PegOut.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid class="exchange normalized-height container
+  max-width mx-6">
+    <v-row class="mx-0">
+      <v-col cols="2" class="d-flex justify-start ma-0 pa-0 mt-10">
+        <v-btn rounded outlined color="#00B520" width="110" @click="back">
+          <span>Back</span>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from 'vue-property-decorator';
+
+@Component({
+})
+export default class PegOut extends Vue {
+  @Emit()
+  back():void {
+    this.$router.push({ name: 'Home' });
+  }
+}
+</script>
