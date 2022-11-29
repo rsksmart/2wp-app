c019fd2c2e875db8fa0155777fbe18843f199228
ronaldsg <ronaldsg20@gmail.com>
2022-11-29 10:26:20 -0500

    Set up form styles

--- src/components/pegout/RskWalletConnection.vue
+++ src/components/pegout/RskWalletConnection.vue
@@ -1,19 +1,25 @@
 <template>
-  <v-container class="form-step">
+  <v-container class="form-step mt-5 mb-0 py-0">
     <v-row align="start mx-0">
       <v-col cols="auto" class="pl-0">
         <div v-bind:class="[focus ?
               'number-filled' : 'number']">1</div>
       </v-col>
-      <v-col class="pl-0 mb-4">
+      <v-col class="pl-0">
         <p v-bind:class="{'boldie': focus}">
           Connect your RSK wallet:
         </p>
         <v-row>
-          <v-btn outlined rounded color="#00B520" width="100%" height="38"
-                 @click="connectWallet" @focus="focus = true" @blur="focus = false">
-            <span class="greenish">Connect wallet</span>
-          </v-btn>
+          <v-col>
+            <v-text-field class="disabled-input" :value="address" disabled color="#F8F5F5"
+                          solo hide-details full-width single-line flat/>
+          </v-col>
+          <v-col class="d-flex align-center">
+            <v-btn rounded color="#00B520" width="100%" height="38"
+                   @click="connectWallet" @focus="focus = true" @blur="focus = false">
+              <span class="whiteish">Connect wallet</span>
+            </v-btn>
+          </v-col>
         </v-row>
       </v-col>
     </v-row>
@@ -22,15 +28,23 @@
 
 <script lang="ts">
 import { Component, Emit, Vue } from 'vue-property-decorator';
+import { State } from 'vuex-class';
+import { PegOutTxState } from '@/types';
 
 @Component({})
 export default class RskWalletConnection extends Vue {
   focus = true;
 
+  @State('pegOutTx') pegOutTxState!: PegOutTxState;
+
   @Emit()
   // eslint-disable-next-line class-methods-use-this
   connectWallet() {
     console.log('connecting to wallet');
+  }
+
+  get address(): string {
+    return this.pegOutTxState.senderAddress ? this.pegOutTxState.senderAddress : 'unset wallet';
   }
 }
 </script>
--- src/types/pegOutTx.ts
+++ src/types/pegOutTx.ts
@@ -12,4 +12,5 @@ export interface PegOutTxState {
   };
   selectedFee: MiningSpeedFee;
   balance: Big;
+  senderAddress?: string;
 }
