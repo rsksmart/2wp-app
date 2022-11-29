b8c398348dea6ccad815315038cbe7dae930b46e
ronaldsg <ronaldsg20@gmail.com>
2022-11-29 03:43:51 -0500

    Add Pegout module
    		I've created an store module for pegout required variables. Also
    		I used the required actions in the created components on pegout form

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Vuex, { StoreOptions } from 'vuex';
-import { RootState } from '../types/store';
+import { RootState } from '@/types';
 import { pegInTx } from './peginTx';
 import { web3Session } from './session';
 import { view } from './view';
 import { status } from './status';
+import { pegOutTx } from './pegoutTx';
 import { version } from '../../package.json';
 
 Vue.use(Vuex);
@@ -21,6 +22,7 @@ const store: StoreOptions<RootState> = {
     web3Session,
     view,
     status,
+    pegOutTx,
   },
 };
 
--- src/views/PegOut.vue
+++ src/views/PegOut.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container fluid class="exchange normalized-height container
-  max-width mx-6">
+  max-width mx-6 pt-6">
+    <peg-out-form/>
     <v-row class="mx-0">
       <v-col cols="2" class="d-flex justify-start ma-0 pa-0 mt-10">
         <v-btn rounded outlined color="#00B520" width="110" @click="back">
@@ -13,8 +14,12 @@
 
 <script lang="ts">
 import { Component, Vue, Emit } from 'vue-property-decorator';
+import PegOutForm from '@/components/pegout/PegOutForm.vue';
 
 @Component({
+  components: {
+    PegOutForm,
+  },
 })
 export default class PegOut extends Vue {
   @Emit()
