29d0775ed39d9781b420fa7e30e1b8481c809c26
ronaldsg <ronaldsg20@gmail.com>
2022-11-29 10:26:20 -0500

    Set up form styles

--- src/store/pegoutTx/actions.ts
+++ src/store/pegoutTx/actions.ts
@@ -1,9 +1,13 @@
 import { ActionTree } from 'vuex';
 import { MiningSpeedFee, PegOutTxState, RootState } from '@/types';
 import * as constants from '@/store/constants';
+import Big from 'big.js';
 
 export const actions: ActionTree<PegOutTxState, RootState> = {
   [constants.PEGOUT_TX_SELECT_FEE_LEVEL]: ({ commit }, feeLevel: MiningSpeedFee) => {
     commit(constants.PEGOUT_TX_SET_SELECTED_FEE_LEVEL, feeLevel);
+  },
+  [constants.PEGOUT_TX_ADD_AMOUNT]: ({ commit }, amountToTransfer: Big) => {
+    commit(constants.PEGOUT_TX_SET_AMOUNT, amountToTransfer);
   },
 };
--- src/store/pegoutTx/mutations.ts
+++ src/store/pegoutTx/mutations.ts
@@ -1,9 +1,13 @@
 import { MutationTree } from 'vuex';
 import { MiningSpeedFee, PegOutTxState } from '@/types';
 import * as constants from '@/store/constants';
+import Big from 'big.js';
 
 export const mutations: MutationTree<PegOutTxState> = {
   [constants.PEGOUT_TX_SET_SELECTED_FEE_LEVEL]: (state, feeLevel: MiningSpeedFee) => {
     state.selectedFee = feeLevel;
+  },
+  [constants.PEGOUT_TX_SET_AMOUNT]: (state, amountToTransfer: Big) => {
+    state.amountToTransfer = amountToTransfer;
   },
 };
