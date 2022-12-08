8b2733c99d8bf3b04c19dedb73097a0dfcca133c
Alex Braz <alexjavabraz@gmail.com>
2022-12-07 22:15:38 -0300

    implement env variables and documentation

--- src/types/environment-variables.ts
+++ src/types/environment-variables.ts
@@ -21,23 +21,31 @@ export class EnvironmentVariables {
 
   public vueAppHotjarId: number;
 
+  public pegoutMinValue: number;
+
+  public pegoutMaxValue: number;
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(defaultValues: any = {}) {
-    this.vueAppCoin = defaultValues.vueAppCoin || process.env.VUE_APP_COIN;
-    this.baseUrl = defaultValues.baseUrl || process.env.BASE_URL;
-    this.vueAppApiBaseUrl = defaultValues.vueAppApiBaseUrl
-      || process.env.VUE_APP_API_BASE_URL;
-    this.vueAppRskNodeHost = defaultValues.vueAppRskNodeHost
-      || process.env.VUE_APP_RSK_NODE_HOST;
-    this.vueAppManifestEmail = defaultValues.vueAppManifestEmail
-      || process.env.VUE_APP_MANIFEST_EMAIL;
-    this.vueAppManifestAppUrl = defaultValues.vueAppManifestAppUrl
-      || process.env.VUE_APP_MANIFEST_APP_URL;
-    this.vueAppWalletAddressHardStop = defaultValues.vueAppWalletAddressHardStop
-      || Number(process.env.VUE_APP_WALLET_ADDRESSES_HARD_STOP);
-    this.vueAppWalletAddressPerCall = defaultValues.vueAppWalletAddressPerCall
-      || Number(process.env.VUE_APP_WALLET_ADDRESS_PER_CALL);
-    this.vueAppRskExplorer = defaultValues.vueAppRskExplorer || process.env.VUE_APP_RSK_EXPLORER;
-    this.vueAppHotjarId = defaultValues.vueAppHotjarId || process.env.VUE_APP_HOTJAR_ID;
+    this.vueAppCoin = process.env.VUE_APP_COIN || defaultValues.vueAppCoin;
+    this.baseUrl = process.env.BASE_URL || defaultValues.baseUrl;
+    this.vueAppApiBaseUrl = process.env.VUE_APP_API_BASE_URL
+      || defaultValues.vueAppApiBaseUrl;
+    this.vueAppRskNodeHost = process.env.VUE_APP_RSK_NODE_HOST
+      || defaultValues.vueAppRskNodeHost;
+    this.vueAppManifestEmail = process.env.VUE_APP_MANIFEST_EMAIL
+      || defaultValues.vueAppManifestEmail;
+    this.vueAppManifestAppUrl = process.env.VUE_APP_MANIFEST_APP_URL
+      || defaultValues.vueAppManifestAppUrl;
+    this.vueAppWalletAddressHardStop = Number(process.env.VUE_APP_WALLET_ADDRESSES_HARD_STOP)
+      || defaultValues.vueAppWalletAddressHardStop;
+    this.vueAppWalletAddressPerCall = Number(process.env.VUE_APP_WALLET_ADDRESS_PER_CALL)
+      || defaultValues.vueAppWalletAddressPerCall;
+    this.vueAppRskExplorer = process.env.VUE_APP_RSK_EXPLORER || defaultValues.vueAppRskExplorer;
+    this.vueAppHotjarId = process.env.VUE_APP_HOTJAR_ID || defaultValues.vueAppHotjarId;
+    this.pegoutMinValue = process.env.PEGOUT_MIN_AMOUNT_ALLOWED_IN_WEI
+      || defaultValues.pegoutMinValue;
+    this.pegoutMaxValue = process.env.PEGOUT_MAX_AMOUNT_ALLOWED_IN_WEI
+      || defaultValues.pegoutMaxValue;
   }
 }
