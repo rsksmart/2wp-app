88b4f3b3c71ee2e0c384a3df9e6fd38beb7e5092
ronaldsg <ronaldsg20@gmail.com>
2023-03-09 11:25:29 -0500

    Generate random nonce vuetify
    		I've updated the nonce definition to a random string in vuetify instance and CSP tag

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import crypto from 'crypto';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
+export const vuetifyNonce = crypto.randomBytes(8).toString('hex');
+
+export const vuetify = new Vuetify({
   theme: {
-    options: { cspNonce: 'dQw4w9WgXcQ' },
+    options: { cspNonce: vuetifyNonce },
   },
 });
