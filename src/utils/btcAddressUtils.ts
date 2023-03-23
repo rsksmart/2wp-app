c720a6970316a6626746ddb03beff0e92655603c
ronaldsg <ronaldsg20@gmail.com>
2023-03-22 22:55:08 -0500

    Update address derivation from message signature
    		I've updated the address derivation method using ethers recovery functions, Also
    		the sign flow was updated using the message hash instead plain text in order to properly derive
    		the destination address

--- src/utils/btcAddressUtils.ts
+++ src/utils/btcAddressUtils.ts
@@ -1,16 +1,19 @@
-import { ecdsaRecover } from 'secp256k1';
 import { Purpose } from '@/types';
 import { EnvironmentAccessorService } from '@/services/enviroment-accessor.service';
 import * as constants from '@/store/constants';
 import * as bitcoin from 'bitcoinjs-lib';
+import {
+  arrayify, computePublicKey, hashMessage, recoverPublicKey,
+} from 'ethers/lib/utils';
 import { deriveAddress, NETWORKS, bitcoinJsNetwork } from './xPubUtils';
 
-export function getPubKeyFromRskSignedMessage(signedMessage:string, hashedMessage: string): Buffer {
-  const hashedMessageBuffer = Buffer.from(hashedMessage.substring(2), 'hex');
-  const messageBuffer = Buffer.from(signedMessage.substring(2, 130), 'hex');
-  const recId = EnvironmentAccessorService.getEnvironmentVariables().vueAppCoin
-  === constants.BTC_NETWORK_MAINNET ? 0 : 1;
-  return Buffer.from(ecdsaRecover(messageBuffer, recId, hashedMessageBuffer, true));
+export function getPubKeyFromRskSignedMessage2(signedMessage:string, hashedMessage: string)
+  : Buffer {
+  const recoveredPk = recoverPublicKey(
+    arrayify(hashMessage(arrayify(hashedMessage))), signedMessage,
+  );
+  const newCompressedPK = computePublicKey(recoveredPk, true).substring(2);
+  return Buffer.from(newCompressedPK, 'hex');
 }
 
 export function getBtcAddressFromSignedMessage(
@@ -18,7 +21,7 @@ export function getBtcAddressFromSignedMessage(
 ): string {
   const network = EnvironmentAccessorService.getEnvironmentVariables().vueAppCoin
   === constants.BTC_NETWORK_MAINNET ? NETWORKS.MAINNET : NETWORKS.TESTNET;
-  const publicKey: Buffer = getPubKeyFromRskSignedMessage(signedMessage, hashedMessage);
+  const publicKey: Buffer = getPubKeyFromRskSignedMessage2(signedMessage, hashedMessage);
   const pubEcpair = bitcoin.ECPair.fromPublicKey(publicKey, { network: bitcoinJsNetwork(network) });
   return deriveAddress({
     purpose: Purpose.P2PKH,
--- tests/unit/utils/btcAddressUtils.spec.ts
+++ tests/unit/utils/btcAddressUtils.spec.ts
@@ -13,14 +13,14 @@ describe('Btc address Derivation', () => {
   it('derives a valid P2PKH address', () => {
     const tests = [
       {
-        signedMessage: '0x0c230d87d3b9984a10c8fa5c1e92af714611e50179fc98b5cc0e049627e94f8d5b0063130be5de459b06a7aa411b6ee269ce99adb0dc1a452cbeb9bd9adabe8c1c',
+        signedMessage: '0x594ac0e8e804214c20594d8dde20058ac887d0a1279980b1db320196da604af6584c24583cd6f7e70f0e44b9b0d7936bf9048b39294c29ed9c24efae8ea4bfc41b',
         hash: '0x34e02cc3794b82c385e42d862f4fc6316713c7d7ce6c61f305756e5c2c2f4581',
-        expectedAddress: 'mghRzECnQWY9jd3P3GS77YvoYbPbXY2mQ1',
+        expectedAddress: 'mkwhnCRC9H7Zy5iCQg9oB8BeRDGZufKmFn',
       },
       {
         signedMessage: '0x93f634d96497e5cfcc8223b0697ddf7a9dd1172872f071b6da25d4fe5069aede54cc8f5a06dbb042cfbfb657030e2feeff6cf254a855862ffe726fd8accf367a1b',
         hash: '0x34e02cc3794b82c385e42d862f4fc6316713c7d7ce6c61f305756e5c2c2f4581',
-        expectedAddress: 'n1uSF2Y2BKNDm2ULRhSmv2hmq3UMiLUiCs',
+        expectedAddress: 'mhiHamqbfTYXuVwyTyVQm9UDiyf3rUaNHp',
       },
     ];
     tests.forEach((test) => {
