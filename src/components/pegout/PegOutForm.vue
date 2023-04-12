4e197ae329cfc6ed469e6dda0a289276cf99088c
Julia Zack <julia.zack@iovlabs.org>
2023-04-12 12:23:34 -0300

    warning & disabled button when liquality added

--- src/components/pegout/PegOutForm.vue
+++ src/components/pegout/PegOutForm.vue
@@ -1,103 +1,98 @@
 <template>
-<v-container fluid class="exchange-form normalized-height container
-  max-width mx-6 mt-6">
-  <v-col class="px-0">
-    <v-row class="mx-0">
+  <v-container fluid class="exchange-form normalized-height container
+                                                                                      max-width mx-6 mt-6">
+    <v-col class="px-0">
+      <v-row class="mx-0">
         <v-col cols="1" class="pa-0 d-flex align-center">
-          <v-img position="center left"
-                 src="@/assets/exchange/arrow.png" height="40" contain/>
+          <v-img position="center left" src="@/assets/exchange/arrow.png" height="40" contain />
         </v-col>
         <v-col class="px-0">
-         <h1 class="justify-center text-left">Send {{environmentContext.getRbtcTicker()}}.
-          Get {{environmentContext.getBtcTicker()}}.</h1>
+          <h1 class="justify-center text-left">Send {{ environmentContext.getRbtcTicker() }}.
+            Get {{ environmentContext.getBtcTicker() }}.</h1>
         </v-col>
       </v-row>
-    <v-row class="exchange-form">
-      <v-col cols="8" lg="8" >
-        <rsk-wallet-connection
-          @switchDeriveButton="switchDeriveButton"/>
-        <v-divider color="#C4C4C4"/>
-        <rbtc-input-amount :enableButton="!isReadyToSign"/>
-        <v-divider color="#C4C4C4"/>
-        <div class="form-step pb-0 pt-3 mb-3">
-          <v-row class="mx-0 align-start">
-            <v-col cols="auto" class="pl-0">
-              <div v-bind:class="[focus ?
-              'number-filled' : 'number']">3</div>
-            </v-col>
-            <v-col class="pl-0 ma-0 pb-0">
-              <p v-bind:class="{'boldie': focus}">
-                (Optional) Verify your Bitcoin destination address:
-              </p>
-              <v-row class="ma-0 mt-2 pa-0">
-                <v-col v-if="session.btcDerivedAddress" cols="7" class="p-0">
-                  <div class="container">
-                    <v-row class="mx-0">
-                      <span>Address derived</span>
+      <v-row class="exchange-form">
+        <v-col cols="8" lg="8">
+          <rsk-wallet-connection @switchDeriveButton="switchDeriveButton" />
+          <v-divider color="#C4C4C4" />
+          <rbtc-input-amount :enableButton="!isReadyToSign" />
+          <v-divider color="#C4C4C4" />
+          <div class="form-step pb-0 pt-3 mb-3">
+            <v-row class="mx-0 align-start">
+              <v-col cols="auto" class="pl-0">
+                <div v-bind:class="[focus ?
+                  'number-filled' : 'number']">3</div>
+              </v-col>
+              <v-col class="pl-0 ma-0 pb-0">
+                <p v-bind:class="{ 'boldie': focus }">
+                  (Optional) Verify your Bitcoin destination address:
+                </p>
+                <v-row class="ma-0 mt-2 pa-0">
+                  <v-col v-if="session.btcDerivedAddress" cols="7" class="p-0">
+                    <div class="container">
+                      <v-row class="mx-0">
+                        <span>Address derived</span>
+                      </v-row>
+                      <v-row class="mx-0 d-flex align-center">
+                        <p class="mb-0 account">
+                          {{ session.btcDerivedAddress }}
+                        </p>
+                      </v-row>
+                    </div>
+                  </v-col>
+                  <v-col v-else cols="4" class="pb-0 px-0">
+                    <v-row class="derive-button ml-1 mx-0 d-flex justify-center">
+                      <v-btn :disabled="!isReadyToSign || injectedProvider == LIQUALITY" outlined rounded width="100%"
+                        height="38" @click="openAddressDialog">
+                        <span>
+                          Get Bitcoin destination address
+                        </span>
+                      </v-btn>
                     </v-row>
-                    <v-row class="mx-0 d-flex align-center">
-                      <p class="mb-0 account">
-                        {{session.btcDerivedAddress}}
-                      </p>
-                    </v-row>
-                  </div>
-                </v-col>
-                <v-col v-else cols="4" class="pb-0 px-0">
-                  <v-row class="derive-button ml-1 mx-0 d-flex justify-center">
-                    <v-btn :disabled="!isReadyToSign"
-                      outlined rounded
-                      width="100%" height="38"
-                      @click="openAddressDialog" >
-                      <span>
-                        Get Bitcoin destination address
-                      </span>
-                    </v-btn>
-                  </v-row>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-        </div>
-      </v-col>
-      <v-col cols="4" lg="4">
-        <tx-summary-fixed
-          :summary="pegOutFormSummary"
-          :initialExpand="true"
-          :type="typeSummary"
-          :orientation="orientationSummary"/>
-      </v-col>
-    </v-row>
-    <v-row v-if="showAddressDialog">
-      <address-dialog @switchDeriveButton="switchDeriveButton"
-       @closeDialog="closeAddressDialog"/>
-    </v-row>
-    <v-row class="mx-0">
-      <v-col cols="2" class="d-flex justify-start ma-0 pa-0">
-        <v-btn @click="back"
-        rounded outlined color="#000000" width="110"
-                :disabled="pegOutFormState.matches(['loading', 'goingHome'])">
-          <span>Back</span>
-        </v-btn>
-      </v-col>
-      <v-col cols="10" class="d-flex justify-end ma-0 py-0 pl-0">
-        <v-btn v-if="!pegOutFormState.matches(['loading'])" rounded color="#000000"
-                @click="send"
-                :disabled="!isReadyToCreate || pegOutFormState.matches(['goingHome'])">
-          <span class="whiteish">Send</span>
-          <v-icon class="ml-2" color="#fff">mdi-send-outline</v-icon>
-        </v-btn>
-        <v-progress-circular v-if="pegOutFormState.matches(['loading'])"
-                              indeterminate color="#000000" class="mr-10"/>
-      </v-col>
-    </v-row>
-  </v-col>
-  <template v-if="showTxErrorDialog">
-    <tx-error-dialog :showTxErrorDialog="showTxErrorDialog"
-                     :errorMessage="txError" @closeErrorDialog="closeTxErrorDialog"/>
-  </template>
-</v-container>
+                  </v-col>
+                  <v-container v-if="injectedProvider === LIQUALITY">
+                    <div>
+                      As you are using Liquality, you need to follow
+                      <a :href=DOC_URL class='d-inline' style='text-decoration: none;' target='_blank'> documentation</a>
+                      to get the address.
+                    </div>
+                  </v-container>
+                </v-row>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+        <v-col cols="4" lg="4">
+          <tx-summary-fixed :summary="pegOutFormSummary" :initialExpand="true" :type="typeSummary"
+            :orientation="orientationSummary" />
+        </v-col>
+      </v-row>
+      <v-row v-if="showAddressDialog">
+        <address-dialog @switchDeriveButton="switchDeriveButton" @closeDialog="closeAddressDialog" />
+      </v-row>
+      <v-row class="mx-0">
+        <v-col cols="2" class="d-flex justify-start ma-0 pa-0">
+          <v-btn @click="back" rounded outlined color="#000000" width="110"
+            :disabled="pegOutFormState.matches(['loading', 'goingHome'])">
+            <span>Back</span>
+          </v-btn>
+        </v-col>
+        <v-col cols="10" class="d-flex justify-end ma-0 py-0 pl-0">
+          <v-btn v-if="!pegOutFormState.matches(['loading'])" rounded color="#000000" @click="send"
+            :disabled="!isReadyToCreate || pegOutFormState.matches(['goingHome'])">
+            <span class="whiteish">Send</span>
+            <v-icon class="ml-2" color="#fff">mdi-send-outline</v-icon>
+          </v-btn>
+          <v-progress-circular v-if="pegOutFormState.matches(['loading'])" indeterminate color="#000000" class="mr-10" />
+        </v-col>
+      </v-row>
+    </v-col>
+    <template v-if="showTxErrorDialog">
+      <tx-error-dialog :showTxErrorDialog="showTxErrorDialog" :errorMessage="txError"
+        @closeErrorDialog="closeTxErrorDialog" />
+    </template>
+  </v-container>
 </template>
-
 <script lang="ts">
 import { Component, Vue, Emit } from 'vue-property-decorator';
 import EnvironmentContextProviderService from '@/providers/EnvironmentContextProvider';
@@ -128,6 +123,12 @@ export default class PegOutForm extends Vue {
   pegOutFormState: Machine<'loading' | 'goingHome' | 'fill'> = new Machine('fill');
 
   environmentContext = EnvironmentContextProviderService.getEnvironmentContext();
+
+  injectedProvider = '';
+
+  LIQUALITY = 'Liquality'
+
+  DOC_URL = 'https://developers.rsk.co/rsk/rbtc/conversion/networks/mainnet/#rbtc-to-btc-conversion'
 
   recipientAddress = '';
 
@@ -165,7 +166,7 @@ export default class PegOutForm extends Vue {
   }
 
   @Emit()
-  back():void {
+  back(): void {
     this.$router.push({ name: 'Home' });
   }
 
@@ -175,6 +176,10 @@ export default class PegOutForm extends Vue {
   }
 
   switchDeriveButton(): void {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.injectedProvider = this.session.rLoginInstance?.providerController.injectedProvider.name;
+    //
     console.log('======================================');
     this.isReadyToSign = !this.isReadyToSign;
   }
@@ -186,7 +191,7 @@ export default class PegOutForm extends Vue {
       .then(() => {
         this.changePage();
       })
-      .catch((error:Error) => {
+      .catch((error: Error) => {
         this.txError = error.message;
         this.showTxErrorDialog = true;
       })
