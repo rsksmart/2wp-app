4ecefcc7aa802bf0595c323e11bf0067a096fdbc
ronaldsg <ronaldsg20@gmail.com>
2023-05-16 08:40:12 -0500

    Pegout left blocks calculation
    		I've defined a getter in the status store module to calculate the estimated
    		block where the btc will be released.

--- src/store/status/getters.ts
+++ src/store/status/getters.ts
@@ -1,9 +1,16 @@
 import { GetterTree } from 'vuex';
 import {
-  RootState, TxStatus, TxStatusMessage, TxStatusType,
+  PegoutStatusDataModel,
+  RootState,
+  TxStatus,
+  TxStatusMessage,
+  TxStatusType,
 } from '@/types';
 import * as constants from '@/store/constants';
 import { setStatusMessage } from '@/services/utils';
+import { BridgeService } from '@/services/BridgeService';
+import Web3 from 'web3';
+import { EnvironmentAccessorService } from '@/services/enviroment-accessor.service';
 
 export const getters: GetterTree<TxStatus, RootState> = {
   [constants.STATUS_IS_REJECTED]: (state: TxStatus): boolean => {
@@ -21,4 +28,31 @@ export const getters: GetterTree<TxStatus, RootState> = {
     const status = state.txDetails ? state.txDetails.status : '';
     return setStatusMessage(state.type, status);
   },
+  [constants.STATUS_GET_ESTIMATED_RELEASE_TIME_IN_MINUTES]: (state: TxStatus)
+    : Promise<number> => new Promise<number>((resolve, reject) => {
+      const bridgeService = new BridgeService();
+      const web3 = new Web3(EnvironmentAccessorService.getEnvironmentVariables().vueAppRskNodeHost);
+      if (state.txDetails) {
+        const status = state.txDetails as PegoutStatusDataModel;
+        web3.eth.getTransaction(status.originatingRskTxHash)
+          .then(({ blockNumber }) => {
+            if (blockNumber) reject(new Error('The tx are not mined yet'));
+            return Promise.all([
+              web3.eth.getBlockNumber(),
+              bridgeService.getNextPegoutCreationBlockAt(blockNumber ?? 0),
+            ]);
+          })
+          .then(([currentBlock, nextPegoutCreationBlock]) => {
+            const estimatedBlocksLeft = nextPegoutCreationBlock
+              + constants.PEGOUT_REQUIRED_CONFIRMATIONS
+              + constants.PEGOUT_SIGNING_BLOCKS_GAP - currentBlock;
+            const estimatedMinutes = estimatedBlocksLeft
+              * Math.floor(constants.BLOCKS_PER_YEAR / (365.25 * 1440));
+            resolve(estimatedMinutes);
+          })
+          .catch(reject);
+      } else {
+        resolve(-1);
+      }
+    }),
 };
