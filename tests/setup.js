e93ed6968f3a67e2a040ed7d100b76d6147a20fc
ronaldsg <ronaldsg20@gmail.com>
2023-05-25 21:55:08 -0500

    Add text encoding polyfill needed to run some tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,16 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import { TextEncoder, TextDecoder } from 'text-encoding';
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
+// Polyfill TextEncoder if it doesn't exist
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
+
+// // Polyfill TextDecoder if it doesn't exist
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder;
+}
