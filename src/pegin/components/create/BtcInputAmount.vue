4d03e5f418e34e81558e61bdb30dcbb7586e1a4d
julia zack <julia.zack@iovlabs.org>
2023-05-31 19:39:25 -0300

    Better approach to handle overlapping

--- src/pegin/components/create/BtcInputAmount.vue
+++ src/pegin/components/create/BtcInputAmount.vue
@@ -14,6 +14,7 @@
                 class="input-box-outline" id="amount-field">
             <v-col cols="8" class="pa-0 pl-1">
               <v-text-field solo hide-details full-width single-line flat
+                            :disabled="isTourActive"
                             class="amount-input"
                             placeholder="add amount"
                             v-model="bitcoinAmount" type="number"
@@ -69,7 +70,7 @@
 
 <script lang="ts">
 import {
-  Component, Emit, Vue, Watch,
+  Component, Emit, Prop, Vue, Watch,
 } from 'vue-property-decorator';
 import { Action, Getter, State } from 'vuex-class';
 import SatoshiBig from '@/common/types/SatoshiBig';
@@ -90,6 +91,8 @@ export default class BtcInputAmount extends Vue {
   bitcoinAmount = '';
 
   stepState: 'unused' | 'done' | 'error' = 'unused';
+
+  @Prop() isTourActive !: boolean;
 
   @State('pegInTx') pegInTxState!: PegInTxState;
 
