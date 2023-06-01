fa0320dc532d2f388ac72982d3f9ebe387186323
julia zack <julia.zack@iovlabs.org>
2023-06-01 17:58:53 -0300

    Remove unused btcConfirmations from peginConf

--- src/pegin/services/peginConfigurationService.ts
+++ src/pegin/services/peginConfigurationService.ts
@@ -5,26 +5,22 @@ export default class PeginConfigurationService {
   public static getPeginConfiguration(): Promise<PeginConfiguration> {
     return new Promise<PeginConfiguration>((resolve, reject) => {
       const bridgeService = new BridgeService();
-
       Promise.all([
         bridgeService.getMinPeginValue(),
         bridgeService.getFederationAddress(),
         bridgeService.getPeginAvailability(),
       ])
-        .then(([minValue, federationAddress, availability]) => {
-          const peginConf: PeginConfiguration = ({
-            minValue,
-            maxValue: availability,
-            federationAddress,
-            btcConfirmations: Number(process.env.BTC_CONFIRMATIONS) ?? 100,
-            // sessionId should be remove eventually
-            sessionId: '',
-          });
-          resolve(peginConf);
-        })
-        .catch((e) => {
-          reject(e);
+      .then(([minValue, federationAddress, availability]) => {
+        const peginConf: PeginConfiguration = ({
+          minValue,
+          maxValue: availability,
+          federationAddress,
+          // sessionId should be remove eventually
+          sessionId: '',
         });
+        resolve(peginConf);
+      })
+      .catch(reject);
     });
   }
 }
