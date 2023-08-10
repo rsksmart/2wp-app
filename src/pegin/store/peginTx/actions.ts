8330bf76dded8a83bbd94a502ea48750f60faa50
Alex Braz <alexjavabraz@gmail.com>
2023-08-10 15:57:54 -0300

    implementing max button

--- src/pegin/store/peginTx/actions.ts
+++ src/pegin/store/peginTx/actions.ts
@@ -54,6 +54,9 @@ export const actions: ActionTree<PegInTxState, RootState> = {
         commit(constants.PEGIN_TX_SET_LOADING_BALANCE, loadingBalance);
       });
   },
+  [constants.PEGIN_TX_INIT]: ({ dispatch }):
+  Promise<void> => dispatch(constants.PEGIN_TX_ADD_BITCOIN_PRICE)
+    .then(() => dispatch(constants.PEGIN_TX_ADD_PEGIN_CONFIGURATION)),
   [constants.PEGIN_TX_ADD_BITCOIN_PRICE]: ({ commit }) => axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then((response: AxiosResponse) => {
       const [result] = response.data;
@@ -62,10 +65,6 @@ export const actions: ActionTree<PegInTxState, RootState> = {
   [constants.PEGIN_TX_CLEAR_STATE]: ({ commit }): void => {
     commit(constants.PEGIN_TX_CLEAR);
   },
-  [constants.PEGIN_TX_INIT]: ({ dispatch }):
-    Promise<void> => dispatch(constants.PEGIN_TX_ADD_BITCOIN_PRICE)
-    .then(() => dispatch(constants.PEGIN_TX_ADD_PEGIN_CONFIGURATION)),
-
   [constants.PEGIN_TX_SELECT_ACCOUNT_TYPE]: ({ commit }, accountType: BtcAccount): void => {
     commit(constants.PEGIN_TX_SET_ACCOUNT_TYPE, accountType);
   },
