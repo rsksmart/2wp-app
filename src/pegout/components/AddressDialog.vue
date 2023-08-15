96e8f1613706e9797050463aec297613a975189e
lserra-iov <lucio.serra@iovlabs.org>
2023-08-15 14:14:01 -0300

    Add pegout components

---
+++ src/pegout/components/AddressDialog.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-dialog v-model="showAddressDialog" width="520" persistent>
+    <v-card class="container dialog">
+      <v-col cols="12" align-self="center" class="pt-0">
+        <v-row class="mx-0 mt-4 mb-4 d-flex justify-center">
+            <p class="pop-up-title">Sign this message</p>
+        </v-row>
+        <v-row class="mx-0 mb-10 justify-center">
+            <p class="pop-up-subtitle mb-4">
+              We will use the signature to derive your Bitcoin address.
+            </p>
+            <p class="mb-2 p-0">
+              This does not expose your data nor spend your funds.
+            </p>
+            <div class="pop-up-sign-field">
+              {{messageToBeSigned}}
+            </div>
+        </v-row>
+        <v-row class="mx-0 mb-10" justify="space-around">
+            <v-btn width="145" height="50" density="compact" variant="outlined" rounded
+            color="#000000" @click="closeDialog">
+                <span class="blackish">Cancel</span>
+            </v-btn>
+            <v-btn width="200" height="50" dense rounded
+            @click="toSign" color="#000000">
+                <span class="whiteish">Sign</span>
+            </v-btn>
+        </v-row>
+      </v-col>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import * as constants from '@/common/store/constants';
+import { useAction } from '@/common/store/helper';
+
+export default defineComponent({
+  name: 'AddressDialog',
+  setup(_, context) {
+    const messageToBeSigned = 'Sign this message to derive your Bitcoin address';
+    const showAddressDialog = ref(true);
+
+    const signMessage = useAction('web3Session', constants.SESSION_SIGN_MESSAGE);
+
+    function closeDialog() {
+      return context.emit('closeDialog', showAddressDialog.value);
+    }
+
+    function toSign() {
+      signMessage(messageToBeSigned)
+        .then(() => {
+          closeDialog();
+        });
+    }
+
+    return {
+      showAddressDialog,
+      messageToBeSigned,
+      closeDialog,
+      toSign,
+    };
+  },
+});
+
+// @Component
+// class AddressDialog extends Vue {
+//   messageToBeSigned = 'Sign this message to derive your Bitcoin address';
+//
+//   showAddressDialog = true;
+//
+//   @Action(constants.SESSION_SIGN_MESSAGE, { namespace: 'web3Session' }) signMessage !: (message: string) => Promise<void>;
+//
+//   @Emit()
+//   toSign() {
+//     this.signMessage(this.messageToBeSigned)
+//       .then(() => {
+//         this.closeDialog();
+//       });
+//   }
+//
+//   @Emit('closeDialog')
+//   closeDialog() {
+//     return this.showAddressDialog;
+//   }
+// }
+</script>
---
+++ src/pegout/components/RskWalletConnection.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="py-4" >
+    <v-row class="align-start ma-0">
+      <v-col cols="auto" class="pl-0">
+        <div v-bind:class="[focus ?
+              'number-filled' : 'number']">1</div>
+      </v-col>
+      <v-col class="pl-0">
+        <p v-bind:class="{'boldie': focus}">
+          Connect your Rootstock wallet:
+        </p>
+        <v-row class="ma-0 mt-4">
+          <template v-if="useWeb3Wallet && web3Address">
+             <div class="pl-1">
+              <v-row class="mx-0">
+                <v-col class="pl-0">
+                   <p class="mb-0 account">
+                     {{ address }} -
+                     {{ balance.toRBTCTrimmedString() }}
+                     {{ environmentContext.getRbtcTicker() }}
+                  </p>
+                </v-col>
+              </v-row>
+              <v-row class="mx-0">
+                <v-col cols="4" class="pa-0 pt-1">
+                  <v-row class="derive-button ma-0 d-flex justify-center">
+                    <v-btn
+                      @click="disconnectWallet"
+                      variant="outlined" rounded
+                      width="100%" height="38">
+                      <span>
+                        Disconnect wallet
+                      </span>
+                    </v-btn>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </div>
+          </template>
+          <template v-else>
+            <v-col cols="7" class="wallet-label-container">
+              <v-row class="input-box-outline-readonly ma-0 pa-0 pl-0" >
+                <v-text-field
+                  v-model="address"
+                  class="wallet-address-input"
+                  density="compact"
+                  variant="solo"
+                  disabled
+                  flat
+                  hide-details
+                  :placeholder="`Connect your wallet to select the ${environmentContext.getRbtcTicker()} address`"
+                  @focus="focus = true"
+                  @blur="focus = false"/>
+              </v-row>
+            </v-col>
+            <v-col cols="1" class="d-flex justify-center pa-0">
+              <div class="divider"/>
+            </v-col>
+            <v-col cols="4" class="pa-0 align-self-center">
+              <v-row class="ma-0 d-flex justify-center">
+                <v-btn variant="outlined" rounded color="#000000" width="100%" height="38"
+                  class="select-wallet-button"
+                  @click="connectWallet" id="wallet-connection">
+                  <span class="blackish">Connect wallet</span>
+                </v-btn>
+              </v-row>
+            </v-col>
+          </template>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, ref, defineComponent } from 'vue';
+import * as constants from '@/common/store/constants';
+import EnvironmentContextProviderService from '@/common/providers/EnvironmentContextProvider';
+import { useAction, useStateAttribute } from '@/common/store/helper';
+import WeiBig from '../../common/types/WeiBig';
+
+export default defineComponent({
+  name: 'RskWalletConnection',
+  setup(_, context) {
+    const focus = ref(false);
+    const useWeb3Wallet = ref(false);
+    const environmentContext = EnvironmentContextProviderService.getEnvironmentContext();
+
+    const account = useStateAttribute<string>('web3Session', 'account');
+    const balance = useStateAttribute<WeiBig>('web3Session', 'balance');
+    const connectWeb3 = useAction('web3Session', constants.SESSION_CONNECT_WEB3);
+    const clearAccount = useAction('web3Session', constants.WEB3_SESSION_CLEAR_ACCOUNT);
+    const getBalance = useAction('web3Session', constants.WEB3_SESSION_ADD_BALANCE);
+
+    const web3Address = computed(() => account.value ?? '');
+
+    const address = computed((): string => (account.value ? account.value : ''));
+
+    function switchSignature() {
+      context.emit('switchDeriveButton', !!account.value);
+    }
+
+    function disconnectWallet() {
+      focus.value = true;
+      clearAccount();
+      switchSignature();
+    }
+
+    function connectWallet(): Promise<void> {
+      useWeb3Wallet.value = true;
+      focus.value = true;
+      return connectWeb3()
+        .then(() => {
+          focus.value = false;
+          getBalance();
+          switchSignature();
+        })
+        .catch(() => {
+          focus.value = false;
+          clearAccount();
+        });
+    }
+
+    return {
+      useWeb3Wallet,
+      web3Address,
+      address,
+      environmentContext,
+      balance,
+      disconnectWallet,
+      focus,
+      connectWallet,
+    };
+  },
+});
+//
+// @Component({})
+// class RskWalletConnection extends Vue {
+//   focus = false;
+//
+//   useWeb3Wallet = false;
+//
+//   isValidPegOutAddress = true;
+//
+//   @State('pegOutTx') pegOutTxState!: PegOutTxState;
+//
+//   @State('web3Session') web3SessionState!: SessionState;
+//
+//   @Action(constants.SESSION_CONNECT_WEB3, { namespace: 'web3Session' }) connectWeb3 !: () => Promise<void>;
+//
+//   @Action(constants.WEB3_SESSION_CLEAR_ACCOUNT, { namespace: 'web3Session' }) clearAccount !: () => void;
+//
+//   @Action(constants.WEB3_SESSION_ADD_BALANCE, { namespace: 'web3Session' }) getBalance !: () => Promise<void>;
+//
+//   environmentContext = EnvironmentContextProviderService.getEnvironmentContext();
+//
+//   get web3Address() {
+//     return this.web3SessionState.account ?? '';
+//   }
+//
+//   disconnectWallet() {
+//     this.focus = true;
+//     this.clearAccount();
+//     this.switchSignature();
+//   }
+//
+//   connectWallet(): Promise<void> {
+//     this.useWeb3Wallet = true;
+//     this.focus = true;
+//     return this.connectWeb3()
+//       .then(() => {
+//         this.focus = false;
+//         this.getBalance();
+//         this.switchSignature();
+//       })
+//       .catch(() => {
+//         this.focus = false;
+//         this.clearAccount();
+//       });
+//   }
+//
+//   @Emit('switchDeriveButton')
+//   switchSignature(): boolean {
+//     return this.web3SessionState.account !== undefined;
+//   }
+//
+//   get address(): string {
+//     return this.web3SessionState.account ? this.web3SessionState.account : '';
+//   }
+// }
+</script>
