7957ad86bdbf6655309867ef1d6449f8d24194b7
Alex Braz <alexjavabraz@gmail.com>
2023-10-02 23:57:02 -0300

    adjusting javascript

--- src/pegout/components/Confirmation.vue
+++ src/pegout/components/Confirmation.vue
@@ -119,9 +119,15 @@ export default class Confirmation extends Vue {
     };
   }
 
-  beforeMount() {
+  afterMount() {
+    const vueAppClarityId = 'ibn9mzxbfg';
     this.scriptTag = document.createElement('script');
     this.scriptTag.type = 'text/javascript';
+    this.scriptTag.text = '(function(c,l,a,r,i,t,y){'
+      + 'c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};'
+      + 't=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;'
+      + 'y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);'
+       + `})(window, document, 'clarity', 'script', '${vueAppClarityId}');`;
     this.scriptTag.text = 'clarity("set", "pegout_tx", "true");';
     document.body.appendChild(this.scriptTag);
   }
