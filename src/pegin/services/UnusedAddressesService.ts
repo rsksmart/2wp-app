9439212f46971de23c9b43fb07dc24832e29ec30
ronaldsg <ronaldsg20@gmail.com>
2023-11-15 14:38:12 -0500

    Include unused address service in the balance flow

--- src/pegin/services/UnusedAddressesService.ts
+++ src/pegin/services/UnusedAddressesService.ts
@@ -1,27 +1,22 @@
-import axios from 'axios';
-import { EnvironmentAccessorService } from '@/common/services/enviroment-accessor.service';
 import { AddressStatus, SatoshiBig } from '@/common/types';
-import { AddressInfo } from '../types';
+import { ApiService } from '@/common/services';
 
 export default class UnusedAddressesService {
-  private static getAddressInfo(address: string): Promise<AddressInfo> {
-    const baseUrl = EnvironmentAccessorService.getEnvironmentVariables().vueAppApiBaseUrl;
-
-    return axios.get(`${baseUrl}/api/v2/address/${address}`)
-      .then((response) => response.data);
-  }
-
   public static areUnusedAddresses(addressList: string[]): Promise<AddressStatus[]> {
-    const addressInfoPromises = addressList.map((address) => this.getAddressInfo(address));
-
-    return Promise.all(addressInfoPromises)
+    return ApiService.getAddressesInfo(addressList)
       .then((addressesInfo) => addressesInfo.map(
         (addressInfo) => {
           const totalReceived = new SatoshiBig(addressInfo.totalReceived, 'satoshi');
           const totalSent = new SatoshiBig(addressInfo.totalSent, 'satoshi');
           const unused = totalReceived.eq(0) && totalSent.eq(0);
-          return { address: addressInfo.address, unused };
+          return {
+            address: addressInfo.address,
+            unused,
+          };
         },
-      )).catch((e) => { throw e; });
+      ))
+      .catch((e) => {
+        throw e;
+      });
   }
 }
