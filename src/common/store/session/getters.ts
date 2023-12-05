6b4b1359fe13cb87add70a54c58a9532c62a6122
lserra-iov <lucio.serra@iovlabs.org>
2023-12-05 11:24:46 -0300

    Fix getter typo

--- src/common/store/session/getters.ts
+++ src/common/store/session/getters.ts
@@ -8,6 +8,6 @@ export const getters: GetterTree<SessionState, RootState> = {
   [constants.SESSION_IS_LEDGER_CONNECTED]: (state): boolean => !!(state.rLogin?.provider.isLedger),
   [constants.SESSION_IS_TREZOR_CONNECTED]: (state): boolean => !!(state.rLogin?.provider.isTrezor),
   [constants.SESSION_IS_METAMASK_CONNECTED]:
-    (state): boolean => !!(state.rLogin?.provider.isMetamask),
+    (state): boolean => !!(state.rLogin?.provider.isMetaMask),
   [constants.SESSION_IS_RLOGIN_DEFINED]: (state): boolean => state.rLogin !== undefined,
 };
