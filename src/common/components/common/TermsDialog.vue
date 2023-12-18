8bb3168c71f6456f6bacc888740031756b300474
lserra-iov <lucio.serra@iovlabs.org>
2023-12-18 12:12:46 -0300

    Check if dialog requires scroll

--- src/common/components/common/TermsDialog.vue
+++ src/common/components/common/TermsDialog.vue
@@ -8,7 +8,7 @@
           <h2>Terms and conditions</h2>
         </v-row>
         <v-row>
-          <v-container class="terms-txt mx-15 my-6 pa-10" @scroll="onScroll">
+          <v-container class="terms-txt mx-15 my-6 pa-10" @scroll="onScroll" ref="scrollableArea">
             <v-row>
               <p>{{ dialogText }}</p>
             </v-row>
@@ -17,7 +17,7 @@
         <v-row class="d-flex justify-end mx-11 my-5">
           <v-btn rounded variant="outlined" color="#000000" width="110"
                  @click="$emit('update:showDialog', false)"
-                 :disabled="!scrolledText">
+                 :disabled="requiresScroll && !scrolledText ">
             <span>Back</span>
           </v-btn>
         </v-row>
@@ -26,7 +26,9 @@
   </template>
 
 <script lang="ts">
-import { ref, defineComponent, computed } from 'vue';
+import {
+  ref, defineComponent, computed, watchEffect,
+} from 'vue';
 import { useStateAttribute } from '@/common/store/helper';
 import { Feature } from '@/common/types';
 
@@ -36,6 +38,7 @@ export default defineComponent({
     showDialog: Boolean,
   },
   setup(props, context) {
+    const areTermsAccepted = useStateAttribute<boolean>('web3Session', 'acceptedTerms');
     const termsAndConditionsEnabled = useStateAttribute<Feature>('web3Session', 'termsAndConditionsEnabled');
     const dialogText = computed(() => termsAndConditionsEnabled.value?.value);
     const show = computed({
@@ -47,6 +50,8 @@ export default defineComponent({
       },
     });
     const scrolledText = ref(false);
+    const scrollableArea = ref();
+    const requiresScroll = ref();
 
     function onScroll(
       event: { target: { scrollTop: number; clientHeight: number; scrollHeight: number; }},
@@ -59,11 +64,22 @@ export default defineComponent({
       }
     }
 
+    watchEffect(() => {
+      if (scrollableArea.value) {
+        const el = scrollableArea.value.$el;
+        requiresScroll.value = el.scrollHeight > el.clientHeight;
+        scrolledText.value = false;
+      }
+    });
+
     return {
       show,
       scrolledText,
       onScroll,
       dialogText,
+      scrollableArea,
+      requiresScroll,
+      areTermsAccepted,
     };
   },
 });
