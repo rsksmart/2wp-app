bb6cf676d8d4533acb9cb2d51058e1a93a1f086f
lserra-iov <lucio.serra@iovlabs.org>
2024-01-18 12:06:12 -0300

    Create LeatherService

--- src/common/services/index.ts
+++ src/common/services/index.ts
@@ -3,3 +3,4 @@ export { default as ApiService } from './ApiService';
 export { default as TrezorService } from './TrezorService';
 export { default as LedgerService } from './LedgerService';
 export { default as LiqualityService } from './LiqualityService';
+export { default as LeatherService } from './LeatherService';
