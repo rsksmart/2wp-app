ea763347d5d53bb43b64d5f22dc46246529044e4
lserra-iov <lucio.serra@iovlabs.org>
2024-01-30 14:43:43 -0300

    Add actions to flyoverPegout store

--- src/pegout/store/FlyoverPegout/mutations.ts
+++ src/pegout/store/FlyoverPegout/mutations.ts
@@ -1,5 +1,23 @@
 import { FlyoverPegoutState } from '@/common/types';
 import { MutationTree } from 'vuex';
+import * as constants from '@/common/store/constants';
+import { getClearFlyoverPegoutState } from '@/common/utils/common';
 
 export const mutations: MutationTree<FlyoverPegoutState> = {
+  [constants.FLYOVER_PEGOUT_SET_SERVICE]: (state, flyoverService) => {
+    state.flyoverService = flyoverService;
+  },
+  [constants.FLYOVER_PEGOUT_SET_PROVIDERS]: (state, providers) => {
+    state.liquidityProviders = providers;
+  },
+  [constants.FLYOVER_PEGOUT_SET_AMOUNT]: (state, amount) => {
+    state.amountToTransfer = amount;
+  },
+  [constants.FLYOVER_PEGOUT_SET_QUOTES]: (state, quotes) => {
+    state.quotes.push(quotes);
+  },
+  [constants.FLYOVER_PEGOUT_SET_CLEAR_STATE]: (state) => {
+    const clearState = getClearFlyoverPegoutState();
+    Object.assign(state, clearState);
+  },
 };
