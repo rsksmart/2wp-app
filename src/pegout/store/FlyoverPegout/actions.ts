4fdc8b46625c44440ef37b0357372a9874ced028
lserra-iov <lucio.serra@iovlabs.org>
2024-01-30 17:22:56 -0300

    Fix getProviders and getQuotes from store

--- src/pegout/store/FlyoverPegout/actions.ts
+++ src/pegout/store/FlyoverPegout/actions.ts
@@ -1,35 +1,39 @@
-import { FlyoverPegoutState, RootState, WeiBig } from '@/common/types';
+import {
+  FlyoverPegoutState, QuotePegOut2WP, RootState, WeiBig,
+} from '@/common/types';
 import { ActionTree } from 'vuex';
 import * as constants from '@/common/store/constants';
-import { FlyoverService } from '@/common/services';
 
 export const actions: ActionTree<FlyoverPegoutState, RootState> = {
-  [constants.FLYOVER_PEGOUT_INIT]: async ({ commit, dispatch }) => {
-    const flyoverService = new FlyoverService();
-    flyoverService.initialize()
-      .then(() => commit(constants.FLYOVER_PEGOUT_SET_SERVICE, flyoverService))
+  [constants.FLYOVER_PEGOUT_INIT]: async ({ state, dispatch }) => {
+    state.flyoverService.initialize()
       .then(() => dispatch(constants.FLYOVER_PEGOUT_GET_PROVIDERS));
   },
   [constants.FLYOVER_PEGOUT_GET_PROVIDERS]: async ({ state, commit }) => {
-    state.flyoverService?.getProviders()
+    state.flyoverService.getProviders()
       .then((providers) => commit(constants.FLYOVER_PEGOUT_SET_PROVIDERS, providers))
-      .catch((e) => console.log(e)); // FIXME
+      .catch((e) => console.log(e));
   },
   [constants.FLYOVER_PEGOUT_ADD_AMOUNT]: ({ commit }, amount: WeiBig) => {
     commit(constants.FLYOVER_PEGOUT_SET_AMOUNT, amount);
   },
   [constants.FLYOVER_PEGOUT_GET_QUOTES]: async ({ state, commit, dispatch }) => {
+    const quotePromises: Promise<QuotePegOut2WP[]>[] = [];
     state.liquidityProviders.forEach((provider) => {
       dispatch(constants.FLYOVER_PEGOUT_USE_LIQUIDITY_PROVIDER, provider.id);
-      state.flyoverService?.getPegoutQuotes(
+      quotePromises.push(state.flyoverService.getPegoutQuotes(
         state.rskRefundAddress,
         state.btcRecipientAddress,
         state.btcRecipientAddress,
         state.amountToTransfer,
-      ).then((quotes) => {
-        commit(constants.FLYOVER_PEGOUT_SET_QUOTES, { [provider.id]: quotes });
-      });
+      ));
     });
+    Promise.all(quotePromises).then((quotes) => quotes.forEach(
+      (providerQuotes, index) => commit(
+        constants.FLYOVER_PEGOUT_SET_QUOTES,
+        { [state.liquidityProviders[index].id]: providerQuotes },
+      ),
+    ));
   },
   [constants.FLYOVER_PEGOUT_USE_LIQUIDITY_PROVIDER]: ({ state }, providerId: number) => {
     state.flyoverService?.useLiquidityProvider(providerId);
