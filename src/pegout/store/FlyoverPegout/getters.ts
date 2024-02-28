2f606c79f5581ca9c1f6ac0c6b02610bf1640f47
lserra-iov <lucio.serra@iovlabs.org>
2024-02-28 14:54:55 -0300

    Validate allowed values for flyover
    Use min and max values from default provider.

--- src/pegout/store/FlyoverPegout/getters.ts
+++ src/pegout/store/FlyoverPegout/getters.ts
@@ -1,6 +1,7 @@
-import { FlyoverPegoutState, RootState } from '@/common/types';
+import { FlyoverPegoutState, RootState, WeiBig } from '@/common/types';
 import { GetterTree } from 'vuex';
 import * as constants from '@/common/store/constants';
+import { EnvironmentAccessorService } from '@/common/services/enviroment-accessor.service';
 
 export const getters: GetterTree<FlyoverPegoutState, RootState> = {
   [constants.FLYOVER_PEGOUT_GET_PROVIDER_ID]: (state) => (quoteHash: string): number => {
@@ -22,5 +23,16 @@ export const getters: GetterTree<FlyoverPegoutState, RootState> = {
       }
     });
     return quoteFound;
+  },
+  [constants.FLYOVER_PEGOUT_GET_MIN_MAX_VALUES]: (state) => {
+    if (state.liquidityProviders.length === 0) {
+      return {
+        minValue: new WeiBig(EnvironmentAccessorService.getEnvironmentVariables().pegoutMinValue, 'rbtc'),
+        maxValue: new WeiBig(EnvironmentAccessorService.getEnvironmentVariables().pegoutMaxValue, 'rbtc'),
+      };
+    }
+    const { pegout } = state.liquidityProviders[0];
+    const { minTransactionValue, maxTransactionValue } = pegout;
+    return { minValue: minTransactionValue, maxValue: maxTransactionValue };
   },
 };
