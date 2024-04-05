7154cb2273c0b504bc6ce88dee8185d27fa4adf6
ronaldsg <ronaldsg20@gmail.com>
2024-04-05 10:28:08 -0500

    Using federation address for default btc address on quotes

--- src/common/providers/types.ts
+++ src/common/providers/types.ts
@@ -11,5 +11,4 @@ export interface EnvironmentContext {
   getRskText: () => string;
   getBtcLedgerAppName: () => string;
   getAddressRegexPattern: () => AddressRegexPattern;
-  getBitcoinAddress: () => string;
 }
