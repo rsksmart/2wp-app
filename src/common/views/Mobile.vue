f1d2e29efbd1b13ccac5a228c443a296d34fa7dd
lserra-iov <lucio.serra@iovlabs.org>
2024-04-29 18:43:19 -0300

    Redesign home

--- src/common/views/Mobile.vue
+++ src/common/views/Mobile.vue
@@ -1,35 +1,18 @@
 <template>
-  <v-container v-if="smAndDown" class="mobile" fluid>
-    <v-row class="mobile-content mx-0">
-      <v-col class="py-0">
-        <v-img position="center center" :src="require('@/assets/logo-rootstock-black.png')"
-               v-bind:height="sm ? 175 : 105" contain/>
-      </v-col>
-      <v-col cols="12" class="text-center d-flex flex-column align-center py-0">
-        <div v-bind:class="[sm ? 'title-warning-sm' : 'title-warning-xs']">
-          <h1>Mobile is not available</h1>
-        </div>
-        <div v-bind:class="[sm ? 'text-warning-sm' : 'text-warning-xs']">
-          <p>The mobile functionality is not currently implemented.
-            Redirect to your computer to use the service.</p>
-          <p>Thank you</p>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="ma-auto text-center pa-12">
+    <h1>Mobile is not available</h1>
+    <p>{{ copy }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { useDisplay } from 'vuetify';
-
 export default {
   name: 'MobileView',
   setup() {
-    const { sm, smAndDown } = useDisplay();
+    const copy = 'The mobile functionality is not currently implemented. Redirect to your computer to use the service. Thank you.';
 
     return {
-      sm,
-      smAndDown,
+      copy,
     };
   },
 };
