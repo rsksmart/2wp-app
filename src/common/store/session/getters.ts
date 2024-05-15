bf6f8f92e77d5708b4fb14b3f29dae1b711eec52
ronaldsg <ronaldsg20@gmail.com>
2024-05-14 21:47:17 -0500

    feature flag pegin
    		I've updated the session module on the store using the feature flags
    		as getter, also including the pegin feature flag on the new design

--- src/common/store/session/getters.ts
+++ src/common/store/session/getters.ts
@@ -2,6 +2,7 @@ import { GetterTree } from 'vuex';
 import * as constants from '@/common/store/constants';
 import { SessionState } from '@/common/types/session';
 import { RootState } from '@/common/types/store';
+import { Feature, FeatureNames } from '@/common/types';
 
 export const getters: GetterTree<SessionState, RootState> = {
   [constants.SESSION_IN_TX_FLOW]: (state): boolean => state.txType !== undefined,
@@ -12,4 +13,7 @@ export const getters: GetterTree<SessionState, RootState> = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (state): boolean => (state.rLoginInstance as any).providerController?.injectedProvider.name
     === constants.RLOGIN_METAMASK_WALLET,
+  [constants.SESSION_IS_RLOGIN_DEFINED]: (state): boolean => state.rLogin !== undefined,
+  [constants.SESSION_GET_FEATURE]: (state) => (feature: FeatureNames)
+  : Feature | undefined => state.features.find((f) => f.name === feature),
 };
