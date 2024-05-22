1326c8258c246b49cfd493dd068df5c150949633
lserra-iov <lucio.serra@iovlabs.org>
2024-05-22 10:02:15 -0300

    Redesign pegout page

--- src/pegout/components/FlyoverPegout.vue
+++ src/pegout/components/FlyoverPegout.vue
@@ -1,92 +1,51 @@
 <template>
   <v-container>
-        <!-- Header -->
-    <v-row class="ma-0">
-      <v-col class="flex-grow-0 align-self-center pl-0 pb-0">
-        <v-img :src="require('@/assets/exchange/arrow.png')" width="40" />
-      </v-col>
-      <v-col class="pb-0">
-        <h1 class="pegout-form-heading">
-          Send {{environmentContext.getRbtcTicker()}}.
-          Get {{environmentContext.getBtcTicker()}}.
-        </h1>
+    <v-row>
+      <v-btn variant="text"
+        :prepend-icon="mdiArrowLeft"
+        @click="back"
+        :disabled="pegOutFormState.matches(['loading', 'goingHome'])">
+        Go Back
+      </v-btn>
+    </v-row>
+    <v-row>
+      <v-col>
+        <flyover-rbtc-input-amount @walletDisconnected="clearState"/>
       </v-col>
     </v-row>
-    <v-row class="exchange-form ma-0 mt-2 justify-space-between">
-      <!-- Form -->
-      <v-col cols="12" class="pa-0">
-        <!-- Step 1 -->
-        <rsk-wallet-connection @switchDeriveButton="switchDeriveButton" class="p"/>
-        <v-divider />
-        <!-- Step 2 -->
-        <flyover-rbtc-input-amount :enableButton="!isReadyToSign"
-        @walletDisconnected="clearState"/>
-        <v-row v-if="!loadingQuotes" class="ma-0 pl-11">
-            <v-btn :disabled="!formFilled"
-              variant="outlined" rounded @click="getQuotes" id="get-quotes-btn">
-              Show options
-            </v-btn>
-        </v-row>
-        <!-- Step 3 -->
-        <v-divider class="mt-4" v-if="showStep && !loadingQuotes"/>
-        <v-row v-if="showStep && !loadingQuotes" class="ma-0 align-start">
-          <v-col cols="auto" class="pl-0">
-            <div :class="[focus ? 'number-filled' : 'number']">3</div>
-          </v-col>
-          <v-col class="pl-0 ma-0 pb-0">
-            <p :class="{'boldie': focus}">
-              Choose the best option to proceed with:
-            </p>
-            <v-container class="px-0">
-              <v-row dense>
-                <v-col cols="6" v-for="(quote, index) in pegoutQuotes" :key="index" >
-                  <pegout-option
-                  :quote="quote"
-                  :formState="pegOutFormState"
-                  :isReadyToSign="isReadyToSign"
-                  :isReadyToCreate="isReadyToCreate"
-                  :pegoutOptionAuthorizedWalletToSign="authorizedWalletToSignMessage"
-                  @openAddressDialog="showAddressDialog = true"
-                  @flyoverInputFocusChanged="handleFlyoverInputFocusChanged"
-                  @send="send(quote.quoteHash)"
-                  />
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-        </v-row>
-        <!-- Fee higher than amount error message -->
-        <div class="form-step py-4 px-4" v-if="!validAmountToReceive && formFilled">
-          <v-row class="alert-msg py-6">
-            <v-col cols="1">
-            <v-icon class="ml-2" color="#DF1B42" :icon="mdiAlertOctagon" size="40"></v-icon>
-            </v-col>
-            <v-col class="px-7">
-              <v-row class="title">
-                The transaction can't be performed
-              </v-row>
-              <v-row class="mt-5">
-                Currently the total fee to pay is higher than the amount you want to send.
-              </v-row>
-            </v-col>
-          </v-row>
-        </div>
+    <span class="d-inline-block font-weight-bold my-3">
+      Speed/Time options
+    </span>
+    <v-btn v-if="flyoverEnabled" @click="getQuotes" class="ml-2">Get quotes</v-btn>
+    <v-row v-if="showStep && !loadingQuotes">
+      <v-col cols="6" v-for="(quote, index) in pegoutQuotes" :key="index" >
+        <pegout-option
+        :class="index === 0 ? 'mr-2' : 'ml-2'"
+        :quote="quote"
+        :isWalletAuthorizedToSign="isWalletAuthorizedToSign"
+        @openAddressDialog="showAddressDialog = true"
+        @changeSelectedOption="changeSelectedOption"
+        :selectedOption="selectedOption === quote.quoteHash"
+        />
       </v-col>
     </v-row>
+    <v-row justify="end">
+        <v-col cols="auto">
+            <v-btn-rsk v-if="!pegOutFormState.matches(['loading'])"
+            @click="send(selectedOption)"
+            :disabled="!isReadyToCreate || pegOutFormState.matches(['goingHome'])"
+            >
+            <template #append>
+              <v-icon :icon="mdiArrowRight" />
+            </template>
+            Continue to Summary
+          </v-btn-rsk>
+          <v-progress-circular v-else indeterminate />
+        </v-col>
+      </v-row>
     <!-- Address Dialog -->
     <v-row v-if="showAddressDialog">
-      <address-dialog @switchDeriveButton="switchDeriveButton"
-                      @closeDialog="showAddressDialog = false"/>
-    </v-row>
-    <!-- Footer -->
-    <v-row class="ma-0 form-footer">
-      <v-col cols="2" class="d-flex justify-start ma-0 pa-0">
-        <v-btn @click="back"
-        rounded variant="outlined" color="#000000" width="110"
-                :disabled="pegOutFormState.matches(['loading', 'goingHome'])">
-          <span>Back</span>
-        </v-btn>
-      </v-col>
+      <address-dialog @closeDialog="showAddressDialog = false"/>
     </v-row>
     <!-- Ledger loading message -->
     <v-row v-if="pegOutFormState.matches(['loading']) && isLedgerConnected"
@@ -124,7 +83,6 @@ import {
 } from 'vue';
 import * as constants from '@/common/store/constants';
 import EnvironmentContextProviderService from '@/common/providers/EnvironmentContextProvider';
-import RskWalletConnection from '@/pegout/components/RskWalletConnection.vue';
 import FlyoverRbtcInputAmount from '@/pegout/components/FlyoverRbtcInputAmount.vue';
 import AddressDialog from '@/pegout/components/AddressDialog.vue';
 import QuoteDiffDialog from '@/pegout/components/QuoteDiffDialog.vue';
@@ -132,11 +90,10 @@ import {
   useAction, useGetter, useState, useStateAttribute,
 } from '@/common/store/helper';
 import { SessionState } from '@/common/types/session';
-import { mdiSendOutline, mdiAlertOctagon } from '@mdi/js';
+import { mdiArrowLeft, mdiArrowRight } from '@mdi/js';
 import {
-  FlyoverPegoutState,
-  NormalizedSummary, ObjectDifference, PegOutTxState, QuotePegOut2WP,
-  SatoshiBig, TxStatusType, TxSummaryOrientation, WeiBig,
+  FlyoverPegoutState, ObjectDifference, PegOutTxState, QuotePegOut2WP,
+  SatoshiBig, TxStatusType, WeiBig,
 } from '@/common/types';
 import { Machine, ServiceError } from '@/common/utils';
 import router from '@/common/router';
@@ -148,7 +105,6 @@ import PegoutOption from './PegoutOption.vue';
 export default defineComponent({
   name: 'FlyoverPegout',
   components: {
-    RskWalletConnection,
     FlyoverRbtcInputAmount,
     AddressDialog,
     PegoutOption,
@@ -156,22 +112,22 @@ export default defineComponent({
     LoadingDialog,
     QuoteDiffDialog,
   },
+  props: {
+    flyoverEnabled: {
+      type: Boolean,
+      required: true,
+    },
+  },
   setup(props, context) {
     const nextPage = 'Confirmation';
-    const typeSummary = TxStatusType.PEGOUT;
-    const orientationSummary = TxSummaryOrientation.VERTICAL;
     const showTxErrorDialog = ref(false);
     const txError = ref<ServiceError>(new ServiceError('', '', '', ''));
     const environmentContext = EnvironmentContextProviderService.getEnvironmentContext();
     const pegOutFormState = ref<Machine<'loading' | 'goingHome' | 'fill'>>(new Machine('fill'));
     const injectedProvider = ref('');
-    const isReadyToSign = ref(false);
-    let authorizedWalletToSignMessage = ref(false);
     const showAddressDialog = ref(false);
-    const flyoverInputFocused = ref(false);
     const loadingQuotes = ref(false);
     const showStep = ref(false);
-    const isValidBtcRecipientAddress = ref(false);
     const showQuoteDiff = ref(false);
     const pegOutTxState = useState<PegOutTxState>('pegOutTx');
     const flyoverPegoutState = useState<FlyoverPegoutState>('flyoverPegout');
@@ -185,11 +141,9 @@ export default defineComponent({
     const selectedQuote = useGetter<QuotePegOut2WP>('flyoverPegout', constants.FLYOVER_PEGOUT_GET_SELECTED_QUOTE);
     const estimatedBtcToReceive = useGetter<SatoshiBig>('pegOutTx', constants.PEGOUT_TX_GET_ESTIMATED_BTC_TO_RECEIVE);
     const isEnoughBalance = useGetter<boolean>('pegOutTx', constants.PEGOUT_TX_IS_ENOUGH_BALANCE);
-    const safeFee = useGetter<WeiBig>('pegOutTx', constants.PEGOUT_TX_GET_SAFE_TX_FEE);
     const isLedgerConnected = useGetter<boolean>('web3Session', constants.SESSION_IS_LEDGER_CONNECTED);
     const isTrezorConnected = useGetter<boolean>('web3Session', constants.SESSION_IS_TREZOR_CONNECTED);
     const isMetamaskConnected = useGetter<boolean>('web3Session', constants.SESSION_IS_METAMASK_CONNECTED);
-    const focus = computed(() => showAddressDialog.value || flyoverInputFocused.value);
 
     const pegoutQuotes = computed(() => {
       const quoteList: QuotePegOut2WP[] = [];
@@ -256,17 +210,11 @@ export default defineComponent({
       return differences;
     });
 
-    function walletAuthorizedToSign() {
-      authorizedWalletToSignMessage = injectedProvider.value === constants.RLOGIN_METAMASK_WALLET
-        || isLedgerConnected.value
-        || session.value.rLogin?.provider.isTrezor;
-    }
+    const isWalletAuthorizedToSign = computed(() => (
+      currentWallet.value !== constants.WALLET_NAMES.LIQUALITY
+    ));
 
     const validAmountToReceive = computed((): boolean => estimatedBtcToReceive.value.gt(0));
-
-    const formFilled = computed((): boolean => !!session.value.account
-      && pegOutTxState.value.amountToTransfer.gt(0)
-      && !!pegOutTxState.value.btcEstimatedFee);
 
     const isReadyToCreate = computed((): boolean => isEnoughBalance.value
         && !!session.value.account
@@ -277,14 +225,6 @@ export default defineComponent({
     function handlePegoutError(error: ServiceError) {
       txError.value = error;
       showTxErrorDialog.value = true;
-    }
-
-    function switchDeriveButton(): void {
-      injectedProvider.value = session.value
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        .rLoginInstance?.providerController.injectedProvider.name;
-      isReadyToSign.value = !isReadyToSign.value;
     }
 
     function changePage(type: string) {
@@ -367,7 +307,6 @@ export default defineComponent({
     }
 
     function getQuotes() {
-      walletAuthorizedToSign();
       loadingQuotes.value = true;
       getPegoutQuotes(session.value.account)
         .catch(handlePegoutError)
@@ -377,31 +316,19 @@ export default defineComponent({
         });
     }
 
-    const pegOutFormSummary = computed((): NormalizedSummary => ({
-      amountFromString: pegOutTxState.value.amountToTransfer.toRBTCTrimmedString(),
-      amountReceivedString: validAmountToReceive.value
-        ? estimatedBtcToReceive.value.toBTCTrimmedString()
-        : '0',
-      fee: Number(pegOutTxState.value.btcEstimatedFee.toBTCTrimmedString()),
-      recipientAddress: session.value.btcDerivedAddress,
-      senderAddress: session.value.account,
-      gas: safeFee.value,
-    }));
+    const selectedOption = ref();
+    function changeSelectedOption(quoteHash: string) {
+      selectedOption.value = quoteHash;
+    }
 
-    function handleFlyoverInputFocusChanged(focused: boolean): void {
-      flyoverInputFocused.value = focused;
+    if (!props.flyoverEnabled) {
+      showStep.value = true;
     }
 
     return {
       environmentContext,
-      switchDeriveButton,
-      isReadyToSign,
       showAddressDialog,
-      authorizedWalletToSignMessage,
-      mdiAlertOctagon,
-      mdiSendOutline,
-      validAmountToReceive,
-      formFilled,
+      isWalletAuthorizedToSign,
       isLedgerConnected,
       pegOutFormState,
       send,
@@ -409,21 +336,18 @@ export default defineComponent({
       isReadyToCreate,
       txError,
       showTxErrorDialog,
-      pegOutFormSummary,
-      typeSummary,
-      orientationSummary,
       pegoutQuotes,
-      focus,
-      handleFlyoverInputFocusChanged,
       clearState,
       getQuotes,
       loadingQuotes,
       sendingPegout,
-      isValidBtcRecipientAddress,
       showStep,
-      flyoverPegoutState,
       quoteDifferences,
       showQuoteDiff,
+      mdiArrowLeft,
+      selectedOption,
+      changeSelectedOption,
+      mdiArrowRight,
     };
   },
 });
