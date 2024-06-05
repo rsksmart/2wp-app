61318e88b6e381e171b92d1b78f07f82dd7f1944
Anni Piragauta <anni.piragauta@iovlabs.org>
2024-06-05 17:10:39 -0500

    refactor: new pegin status

--- src/common/components/status/StatusProgressBar.vue
+++ src/common/components/status/StatusProgressBar.vue
@@ -25,12 +25,6 @@
       <v-col class="px-0">
         <v-progress-linear color="green" :model-value="timeLineData[3][1]" />
       </v-col>
-      <v-col cols="auto" class="pt-2 px-1">
-        <v-img :src="require('@/assets/status/ellipse.svg')" width="12" height="12" contain/>
-      </v-col>
-      <v-col class="px-0">
-        <v-progress-linear color="green" :model-value="timeLineData[4][1]" />
-      </v-col>
       <v-col cols="auto" class="pt-0">
         <v-img class="d-flex flex-0-0" :src="finalStepImage" width="32" height="32" contain/>
       </v-col>
@@ -55,23 +49,23 @@
       <v-col class="pa-0">
         <p class="text-center text-bw-400">{{ timeLineData[4][0] }}</p>
       </v-col>
-      <v-spacer />
-      <v-col class="pa-0">
-        <p class="text-center text-bw-400">{{ timeLineData[5][0] }}</p>
-      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { useState, useStateAttribute } from '@/common/store/helper';
 import {
-  PegoutStatusDataModel,
+  BtcPeginStatus,
+  PeginStatus,
   PegoutStatus,
-  TxStatusType,
+  PegoutStatusDataModel,
+  RskPeginStatus, RskStatus,
   TxStatus,
+  TxStatusType,
 } from '@/common/types';
+import { PegStatus } from '@/common/store/constants';
 
 export default defineComponent({
   name: 'StatusProgressBar',
@@ -80,7 +74,7 @@ export default defineComponent({
   },
   setup(props) {
     const status = useState<TxStatus>('status');
-    const txDetails = useStateAttribute<PegoutStatusDataModel>('status', 'txDetails');
+    const txDetails = useStateAttribute<PegoutStatusDataModel|PeginStatus>('status', 'txDetails');
     const isPegOut = computed((): boolean => status.value.type === TxStatusType.PEGOUT
       || status.value.type === TxStatusType.FLYOVER_PEGOUT);
 
@@ -97,18 +91,19 @@ export default defineComponent({
       return require('@/assets/status/btc.svg');
     });
     const timeLineData = computed(() => {
-      const label = 'step';
+      let labelOne = 'step';
+      let labelTwo = 'step';
+      let labelThree = 'step';
       let zero = 0;
       let first = 0;
       let second = 0;
       let third = 0;
       let fourth = 0;
-      let fifth = 0;
       if (isPegOut.value) {
         if (props.isFlyover) {
           zero = 50;
         } else {
-          switch (txDetails.value.status) {
+          switch (txDetails.value.status as PegoutStatus) {
             case PegoutStatus.PENDING:
               zero = 60;
               break;
@@ -130,7 +125,6 @@ export default defineComponent({
               second = 100;
               third = 100;
               fourth = 100;
-              fifth = 100;
               break;
             default:
               zero = 0;
@@ -138,17 +132,60 @@ export default defineComponent({
               second = 0;
               third = 0;
               fourth = 0;
-              fifth = 0;
+              labelOne = 'step';
+              labelTwo = 'step';
+              labelThree = 'step';
           }
+        }
+      } else {
+        const txDetailsPegIn = txDetails.value as PeginStatus;
+        const btc = txDetailsPegIn.btc as BtcPeginStatus;
+        const rsk = txDetailsPegIn.rsk as RskPeginStatus;
+        labelOne = 'Transaction Broadcasted';
+        labelTwo = 'Transaction Confirmed';
+        labelThree = 'Confirming on Rootstock';
+        switch (txDetailsPegIn.status as PegStatus) {
+          case PegStatus.NOT_IN_BTC_YET:
+            zero = 60;
+            break;
+          case PegStatus.WAITING_CONFIRMATIONS:
+            if (btc.confirmations >= btc.requiredConfirmation) {
+              first = 100;
+            }
+            zero = 100;
+            first = 60;
+            break;
+          case PegStatus.NOT_IN_RSK_YET:
+            zero = 100;
+            first = 100;
+            second = 60;
+            break;
+          case PegStatus.CONFIRMED:
+            if (rsk.status === RskStatus.LOCKED) {
+              third = 100;
+            }
+            zero = 100;
+            first = 100;
+            second = 100;
+            break;
+          default:
+            zero = 0;
+            first = 0;
+            second = 0;
+            third = 0;
+            fourth = 0;
+            labelOne = 'step';
+            labelTwo = 'step';
+            labelThree = 'step';
+            break;
         }
       }
       return {
         0: [isPegOut.value ? 'Bitcoin Network' : 'Rootstock Network', zero],
-        1: [label, first],
-        2: [label, second],
-        3: [label, third],
-        4: [label, fourth],
-        5: [isPegOut.value ? 'Rootstock Network' : 'Bitcoin Network', fifth],
+        1: [labelOne, first],
+        2: [labelTwo, second],
+        3: [labelThree, third],
+        4: [isPegOut.value ? 'Rootstock Network' : 'Bitcoin Network', fourth],
       };
     });
     return {
