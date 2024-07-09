f8b97d0fe3d69556ac411b790e42ceefe9395030
Anni Piragauta <anni.piragauta@iovlabs.org>
2024-07-09 10:07:18 -0500

    fix: sonar cloud Security Hotspots

---
+++ src/common/components/common/TermsContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, h } from 'vue';
+import { useGetter } from '@/common/store/helper';
+import { Feature, FeatureNames } from '@/common/types';
+import * as constants from '@/common/store/constants';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const md = require('markdown-it')();
+
+export default defineComponent({
+  name: 'TermsContent',
+  setup() {
+    const getFeature = useGetter<(f:FeatureNames) =>Feature>('web3Session', constants.SESSION_GET_FEATURE);
+    return () => h('div', {
+      innerHTML: md.render(getFeature.value(FeatureNames.TERMS_AND_CONDITIONS).value),
+    });
+  },
+});
+</script>
