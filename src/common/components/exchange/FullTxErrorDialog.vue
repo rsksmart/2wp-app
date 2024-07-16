50d5579d5670c439529bffbd846c69555ea9f13c
Anni Piragauta <anni.piragauta@iovlabs.org>
2024-07-16 11:56:49 -0500

    refactor: modals and tx error modals styled to new app version

--- src/common/components/exchange/FullTxErrorDialog.vue
+++ src/common/components/exchange/FullTxErrorDialog.vue
@@ -2,40 +2,36 @@
     <v-dialog v-model="show" width="470" persistent>
       <v-card class="container dialog">
         <v-row class="mx-0 mt-6 d-flex justify-center">
-          <v-img :src="require('@/assets/warning.png')" height="47" contain />
+          <v-img :src="require('@/assets/warning.svg')" height="47" contain />
         </v-row>
         <v-row class="mx-0 mt-6 mb-3 d-flex justify-center">
-          <h2>ERROR ON TRANSACTION</h2>
+          <h2 class="bg-purple px-4">Error on Transaction</h2>
         </v-row>
-        <v-col cols="12" align-self="center" class="pt-0">
-            <v-row>
-                <v-col offset="2" cols="8">
-                    <p class="justify-center">{{ error.userMessage }}</p>
-                </v-col>
-            </v-row>
-            <v-row v-if="showDevError">
-                <v-col offset="2" cols="8">
-                    <p class="justify-center">{{ error.technicalMessage }}</p>
-                </v-col>
-            </v-row>
-            <v-row v-if="showDevError">
-                <v-col offset="2" cols="8">
-                    <p class="justify-center">
-                        Service: {{ error.serviceName }} - Method: {{ error.triggeredByMethod }}
-                    </p>
-                </v-col>
-            </v-row>
-            <v-row class="ma-0 my-4" justify="space-around">
-                <v-btn width="200" height="50" variant="outlined" rounded color="#000000"
-                @click="toExchange">
-                <span class="blackish">Return to home</span>
-                </v-btn>
-                <v-btn width="145" height="50"
-                dense rounded color="#000000" @click="closeErrorDialog">
-                <span class="whiteish">Close</span>
-                </v-btn>
-            </v-row>
-        </v-col>
+        <v-row>
+            <v-col offset="2" cols="8">
+                <p class="justify-center">{{ error.userMessage }}</p>
+            </v-col>
+        </v-row>
+        <v-row v-if="showDevError">
+            <v-col offset="2">
+                <p class="justify-center">{{ error.technicalMessage }}</p>
+            </v-col>
+        </v-row>
+        <v-row v-if="showDevError">
+            <v-col offset="2">
+                <p class="justify-center">
+                    Service: {{ error.serviceName }} - Method: {{ error.triggeredByMethod }}
+                </p>
+            </v-col>
+        </v-row>
+        <v-row class="ma-0 my-8" justify="space-around">
+            <v-btn-rsk @click="toExchange">
+              <span>Return to home</span>
+            </v-btn-rsk>
+            <v-btn-rsk  @click="closeErrorDialog">
+              <span>Close</span>
+            </v-btn-rsk>
+        </v-row>
       </v-card>
     </v-dialog>
   </template>
--- src/pegout/components/QuoteDiffDialog.vue
+++ src/pegout/components/QuoteDiffDialog.vue
@@ -2,11 +2,11 @@
     <v-dialog v-model="showWarningMessage" width="870" persistent>
       <v-card class="container dialog">
         <v-row class="mx-0 mt-7 mb-2 d-flex justify-center">
-          <h2>Option Update</h2>
+          <h2 class="bg-purple px-2">Option Update</h2>
         </v-row>
         <v-col cols="12" align-self="center" class="pt-0">
           <v-row class="ma-0 d-flex justify-center">
-            <v-icon class="ml-2" color="#000000" :icon="mdiAlertOutline" size="80"></v-icon>
+            <v-img :src="require('@/assets/warning.svg')" height="64" contain />
           </v-row>
           <v-col offset="1" cols="10">
             <p class="justify-center">
@@ -39,9 +39,9 @@
           </v-col>
           <v-row class="mx-0 mb-8 mt-3" justify="space-around">
             <v-col class="d-flex justify-center">
-              <v-btn width="100" height="40" dense outlined rounded color="#000000" @click="send">
-                <span class="whiteish">continue</span>
-              </v-btn>
+              <v-btn-rsk @click="send">
+                <span>continue</span>
+              </v-btn-rsk>
             </v-col>
           </v-row>
         </v-col>
