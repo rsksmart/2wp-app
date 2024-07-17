155ef5ec74f5cfc99b205e8f3a1cb9bcb1ec30b0
lserra-iov <lucio.serra@iovlabs.org>
2024-07-17 18:10:50 -0300

    Set min/max pegin allowed amounts from env vars

--- src/pegin/services/peginConfigurationService.ts
+++ src/pegin/services/peginConfigurationService.ts
@@ -1,25 +1,25 @@
 import { BridgeService } from '@/common/services/BridgeService';
+import { EnvironmentAccessorService } from '@/common/services/enviroment-accessor.service';
 import { PeginConfiguration } from '@/common/types';
 
 export default class PeginConfigurationService {
   public static getPeginConfiguration(): Promise<PeginConfiguration> {
     return new Promise<PeginConfiguration>((resolve, reject) => {
       const bridgeService = new BridgeService();
-      Promise.all([
-        bridgeService.getMinPeginValue(),
-        bridgeService.getFederationAddress(),
-        bridgeService.getPeginAvailability(),
-      ])
-        .then(([minValue, federationAddress, availability]) => {
-          const peginConf: PeginConfiguration = ({
-            minValue,
-            maxValue: availability,
-            federationAddress,
-            // sessionId should be removed eventually
-            sessionId: '',
-          });
-          resolve(peginConf);
-        })
+      const {
+        peginMinAmountAllowedInBtc: minValue,
+        peginMaxAmountAllowedInBtc: maxValue,
+      } = EnvironmentAccessorService.getEnvironmentVariables();
+      bridgeService.getFederationAddress().then((federationAddress) => {
+        const peginConf: PeginConfiguration = ({
+          minValue,
+          maxValue,
+          federationAddress,
+          // sessionId should be removed eventually
+          sessionId: '',
+        });
+        resolve(peginConf);
+      })
         .catch(reject);
     });
   }
