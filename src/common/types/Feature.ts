c7e21d941cb085f24f484516be643ec323a8c948
lserra-iov <lucio.serra@iovlabs.org>
2024-10-10 15:21:37 -0300

    Show pegin wallets based on feature flags

--- src/common/types/Feature.ts
+++ src/common/types/Feature.ts
@@ -1,7 +1,13 @@
 export enum FeatureNames {
   TERMS_AND_CONDITIONS = 'terms_and_conditions',
   FLYOVER_PEG_IN = 'flyover_pegin',
-  FLYOVER_PEG_OUT = 'flyover_pegout'
+  FLYOVER_PEG_OUT = 'flyover_pegout',
+  WALLET_EXODUS = 'wallet_exodus',
+  WALLET_ENKRYPT = 'wallet_enkrypt',
+  WALLET_XVERSE = 'wallet_xverse',
+  WALLET_LEATHER = 'wallet_leather',
+  WALLET_TREZOR = 'wallet_trezor',
+  WALLET_LEDGER = 'wallet_ledger',
 }
 export interface Feature {
   name: FeatureNames;
