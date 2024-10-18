60c3e17fa087b74e0d6ab01c1a86141922390c72
ronaldsg <ronaldsg20@gmail.com>
2024-10-18 10:36:44 -0500

    Refactor and clean un used data and old code

--- src/pegout/views/PegOut.vue
+++ src/pegout/views/PegOut.vue
@@ -1,43 +1,27 @@
 <template>
   <v-container fluid class="exchange container max-width">
-    <component :is="currentComponent"
-    :flyoverEnabled="flyoverEnabled"
-    :confirmTxState="confirmTxState"
-    @changePage="changePage"/>
+    <pegout-form
+      :flyoverEnabled="flyoverEnabled"/>
   </v-container>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent, onBeforeMount } from 'vue';
 import * as constants from '@/common/store/constants';
-import FlyoverPegout from '@/pegout/components/FlyoverPegout.vue';
-import { Machine } from '@/common/utils';
+import PegoutForm from '@/pegout/components/PegoutForm.vue';
 import { useAction, useGetter } from '@/common/store/helper';
 import { Feature, FeatureNames } from '@/common/types';
 
 export default defineComponent({
   name: 'PegOut',
   components: {
-    FlyoverPegout,
+    PegoutForm,
   },
   setup() {
-    const currentComponent = ref('FlyoverPegout');
-    const confirmTxState = ref<Machine<
-      'idle'
-      | 'loading'
-      | 'error'
-      | 'goingHome'
-      > >(new Machine('idle'));
     const init = useAction('pegOutTx', constants.PEGOUT_TX_INIT);
     const initFlyover = useAction('flyoverPegout', constants.FLYOVER_PEGOUT_INIT);
-    const getBalance = useAction('web3Session', constants.WEB3_SESSION_ADD_BALANCE);
     const flyoverFeature = useGetter<(name: FeatureNames) => Feature>('web3Session', constants.SESSION_GET_FEATURE);
     const flyoverEnabled = ref(false);
-
-    function changePage(componentName: string) {
-      currentComponent.value = componentName;
-      window.scrollTo(0, 0);
-    }
 
     onBeforeMount(() => {
       const feature = flyoverFeature.value(FeatureNames.FLYOVER_PEG_OUT);
@@ -48,12 +32,8 @@ export default defineComponent({
     });
 
     init();
-    getBalance();
 
     return {
-      currentComponent,
-      confirmTxState,
-      changePage,
       flyoverEnabled,
     };
   },
