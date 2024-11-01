6b110b32fc87c581b8d93ac698011c766c869e7e
Anni Piragauta <anni.piragauta@iovlabs.org>
2024-11-01 17:48:23 -0500

    fix: pegout getProviders as promise and added loader

--- src/pegout/views/PegOut.vue
+++ src/pegout/views/PegOut.vue
@@ -1,12 +1,22 @@
 <template>
   <v-container fluid class="exchange container max-width">
-    <pegout-form
-      :flyoverEnabled="flyoverEnabled"/>
+    <div v-if="loadingProviders"
+      class="d-flex justify-center"
+      style="position: absolute; top: 40%; left: 50%; transform: translate(-50%, -50%);">
+        <v-progress-circular
+          :size="250"
+          :width="18"
+          color="warning"
+          indeterminate />
+    </div>
+    <pegout-form v-else :flyoverEnabled="flyoverEnabled" />
   </v-container>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, onBeforeMount } from 'vue';
+import {
+  ref, defineComponent, onBeforeMount,
+} from 'vue';
 import * as constants from '@/common/store/constants';
 import PegoutForm from '@/pegout/components/PegoutForm.vue';
 import { useAction, useGetter } from '@/common/store/helper';
@@ -22,6 +32,7 @@ export default defineComponent({
     const initFlyover = useAction('flyoverPegout', constants.FLYOVER_PEGOUT_INIT);
     const flyoverFeature = useGetter<(name: FeatureNames) => Feature>('web3Session', constants.SESSION_GET_FEATURE);
     const flyoverEnabled = ref(false);
+    const loadingProviders = ref(true);
 
     onBeforeMount(() => {
       const feature = flyoverFeature.value(FeatureNames.FLYOVER_PEG_OUT);
@@ -29,17 +40,20 @@ export default defineComponent({
         initFlyover()
           .then(() => {
             flyoverEnabled.value = true;
+            loadingProviders.value = false;
           })
           .catch(() => {
             flyoverEnabled.value = false;
+            loadingProviders.value = false;
           });
-      }
+      } else loadingProviders.value = false;
     });
 
     init();
 
     return {
       flyoverEnabled,
+      loadingProviders,
     };
   },
 });
