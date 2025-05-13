47c9a4283423116e590e6ab210528c76176ad027
ronaldsg <ronaldsg20@gmail.com>
2025-05-13 01:07:24 -0500

    Fix web3 connect modal logic for wallet selection

--- src/common/components/exchange/Web3WalletDialog.vue
+++ src/common/components/exchange/Web3WalletDialog.vue
@@ -1,51 +1,54 @@
 <template>
-  <v-dialog v-model="show" width="470" persistent>
-    <v-card class="container dialog">
-      <v-row class="mx-0 mt-3 mb-3 d-flex justify-center">
-        <h2>Select your wallet type</h2>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn>
-            Hardware Wallet
-          </v-btn>
-        </v-col>
-        <v-col>
-          <v-btn>
-            Software Wallet
-          </v-btn>
-        </v-col>
-      </v-row>
+  <v-dialog :model-value="modelValue" @update:model-value="emitClose" max-width="400">
+    <v-card>
+      <v-card-title class="d-flex justify-space-between align-center">
+        Selecciona tu Wallet
+        <v-btn icon @click="emitClose" variant="text">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="d-flex flex-column gap-4">
+        <v-btn color="success" @click="selectWallet(constants.WalletTypes.HARDWARE)">
+          Hardware Wallet
+        </v-btn>
+        <v-btn color="info" @click="selectWallet(constants.WalletTypes.SOFTWARE)">
+          Software Wallet</v-btn>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 import EnvironmentContextProviderService from '@/common/providers/EnvironmentContextProvider';
+import * as constants from '@/common/store/constants';
 
 export default defineComponent({
   name: 'Web3WalletDialog',
   props: {
-    showDialog: Boolean,
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
   },
-  setup(props, context) {
-    const show = ref(props.showDialog);
+  setup(props, { emit }) {
     const environmentContext = EnvironmentContextProviderService.getEnvironmentContext();
 
-    function send() {
-      context.emit('continue', props.showDialog);
+    function emitClose() {
+      emit('update:modelValue', false);
     }
 
-    function cancel() {
-      context.emit('cancel', props.showDialog);
+    function selectWallet(wallet: string) {
+      emitClose();
+      emit('selected-wallet', wallet);
     }
 
     return {
       environmentContext,
-      send,
-      cancel,
-      show,
+      emitClose,
+      selectWallet,
+      constants,
     };
   },
 });
