b761ce3e8d7825aad6e19f0e5f5f062e2629efdc
ronaldsg <ronaldsg20@gmail.com>
2025-05-23 01:31:05 -0500

    Refactor hybrid connection modal with designed approach

--- src/common/components/exchange/Web3WalletDialog.vue
+++ src/common/components/exchange/Web3WalletDialog.vue
@@ -1,19 +1,47 @@
 <template>
-  <v-dialog :model-value="modelValue" @update:model-value="emitClose" max-width="400">
-    <v-card>
-      <v-card-title class="d-flex justify-space-between align-center">
-        Selecciona tu Wallet
+  <v-dialog :model-value="modelValue" @update:model-value="emitClose"
+    max-width="450">
+    <v-card min-height="300">
         <v-btn icon @click="emitClose" variant="text">
           <v-icon>mdi-close</v-icon>
         </v-btn>
+      <v-card-title class="d-flex justify-space-between align-center ">
+        <v-row class="d-flex justify-center align-center">
+          <span class="font-weight-bold the-text-h5">
+            Selecciona tu Wallet
+          </span>
+        </v-row>
       </v-card-title>
 
-      <v-card-text class="d-flex flex-column gap-4">
-        <v-btn color="success" @click="selectWallet(constants.WalletTypes.HARDWARE)">
-          Hardware Wallet
-        </v-btn>
-        <v-btn color="info" @click="selectWallet(constants.WalletTypes.SOFTWARE)">
-          Software Wallet</v-btn>
+      <v-card-text class="d-flex flex-column gap-4 my-4">
+        <v-col class="my-4">
+          <v-row class ="d-flex justify-center mb-12 pa-6">
+            <v-btn variant="text"
+              @click="selectWallet(constants.WalletTypes.SOFTWARE)">
+              <div class="bg-bw-400 w-100 h-100 pa-4">
+                <v-icon color="off-white" :icon="mdiCellphoneLink" size="40"></v-icon>
+              </div>
+              <span class="mx-4 text-body-1 font-weight-bold">
+                Software Wallet
+              </span>
+            </v-btn>
+          </v-row>
+          <v-row class="d-flex justify-center mt-12">
+            <v-btn variant="text" @click="selectWallet(constants.WalletTypes.HARDWARE)">
+              <div class="bg-bw-400 w-100 h-100 pa-4">
+                <v-icon color="off-white" :icon="mdiUsbFlashDriveOutline" size="40"></v-icon>
+              </div>
+              <span class="mx-4 text-body-1 font-weight-bold">
+                Hardware Wallet
+              </span>
+            </v-btn>
+          </v-row>
+        </v-col>
+        <v-row class="mt-15 d-flex justify-center">
+          <span>
+            <a href="https://www.rsk.co/rbtc/" target="_blank" class="text-bw-400">Dont't have a wallet? </a>
+          </span>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -23,6 +51,7 @@
 import { defineComponent } from 'vue';
 import EnvironmentContextProviderService from '@/common/providers/EnvironmentContextProvider';
 import * as constants from '@/common/store/constants';
+import { mdiCellphoneLink, mdiUsbFlashDriveOutline } from '@mdi/js';
 
 export default defineComponent({
   name: 'Web3WalletDialog',
@@ -49,6 +78,8 @@ export default defineComponent({
       emitClose,
       selectWallet,
       constants,
+      mdiCellphoneLink,
+      mdiUsbFlashDriveOutline,
     };
   },
 });
