f8bf9762370c9cdba6125f77e2fa8dd88772d51c
ronaldsg <ronaldsg20@gmail.com>
2025-06-25 15:25:33 -0500

    Base PWA settings for IndexedDB use

--- src/pegin/services/index.ts
+++ src/pegin/services/index.ts
@@ -2,3 +2,4 @@ export { default as TxFeeService } from './TxFeeService';
 export { default as PeginTxService } from './PeginTxService';
 export { default as UnusedAddressesService } from './UnusedAddressesService';
 export { default as BalanceService } from './BalanceService';
+export { default as FireblocksService } from './FireblocksService';
