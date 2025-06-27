c1f3b56fbca7cc61633d9ba28fa1e1cdc18dd3a1
ronaldsg <ronaldsg20@gmail.com>
2025-06-27 10:04:59 -0500

    Fix Base url path on FireblocksService

--- src/pegin/services/FireblocksService.ts
+++ src/pegin/services/FireblocksService.ts
@@ -1,3 +1,4 @@
+import { ApiService } from '@/common/services';
 import { FireblocksTransactionParams, VaultAccount } from '@/common/types';
 import axios from 'axios';
 
@@ -8,7 +9,10 @@ export default class FireblocksService {
 
   private cert: string;
 
+  private apiUrl: string;
+
   constructor(apiKey: string, cert: string) {
+    this.apiUrl = `${ApiService.baseURL}/fireblocks`;
     this.basePath = 'https://api.fireblocks.io/v1';
     this.apiKey = apiKey;
     this.cert = cert;
@@ -17,7 +21,7 @@ export default class FireblocksService {
   // eslint-disable-next-line class-methods-use-this
   public async getVaultAccounts(): Promise<Array<VaultAccount>> {
     const encodedSecretKey = btoa(this.cert);
-    const res = await axios.post('http://localhost:3000/fireblocks/vaults', {
+    const res = await axios.post(`${ApiService.baseURL}/fireblocks/vaults`, {
       apiKey: this.apiKey,
       cert: encodedSecretKey,
       options: {
@@ -30,7 +34,7 @@ export default class FireblocksService {
 
   public async sendTransaction(params: FireblocksTransactionParams) {
     const encodedSecretKey = btoa(this.cert);
-    const res = await axios.post('http://localhost:3000/fireblocks/transaction', {
+    const res = await axios.post(`${this.apiUrl}/transaction`, {
       apiKey: this.apiKey,
       cert: encodedSecretKey,
       payload: params,
