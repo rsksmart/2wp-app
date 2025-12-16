5fa3966a65fb00e940b7892d0cf9466a500f31ee
ronaldsg <ronaldsg20@gmail.com>
2025-12-16 18:36:17 +0100

    Enable Max button

--- src/pegin/services/TxFeeService.ts
+++ src/pegin/services/TxFeeService.ts
@@ -42,7 +42,8 @@ export default class TxFeeService {
             selectedUtxoList,
           });
         })
-        .catch(() => {
+        .catch((e) => {
+          console.log(e);
           reject(new ServiceError('TxFeeService', 'getTxFee', 'Something went wrong. Please check your network connection and try again.', 'Unable to get estimated fee from API'));
         });
     });
